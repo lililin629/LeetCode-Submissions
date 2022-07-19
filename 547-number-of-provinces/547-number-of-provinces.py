285461171dfa63e4c1c10e1d2a0165b39c5b0799
lililin629 <42260704+lililin629@users.noreply.github.com>
2022-07-19 11:25:02 +0800

    Time: 202 ms (90.54%), Space: 14.5 MB (36.72%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        ans = self.bfs(isConnected)
+        return ans
+    def bfs(self, M):
+        visited = {i:False for i in range(len(M))}
+        print(visited)
+        ans = 0
+        for i in range(len(M)):
+            if visited[i] == False:
+                ans += 1
+                visited[i] = True
+                q = collections.deque()
+                q.append(i)
+                while q:
+                    now = q.popleft()
+                    for j in range(len(M)):
+                        if M[now][j] == 1 and visited[j] == False:
+                            q.append(j)
+                            visited[j] = True
+        return ans
+    
+        
