0fed6d6afa7c0ede8635f2546e2f6a7fbdf96e33
lililin629 <42260704+lililin629@users.noreply.github.com>
2022-07-19 18:16:48 +0800

    Time: 1433 ms (7.50%), Space: 18.3 MB (20.19%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,26 @@
+import collections
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        new_dict = collections.defaultdict(list)
+        l = len(endWord)
+        for word in wordList:
+            for i in range(l):
+                new_dict[word[:i] + "*" + word[i+1:]].append(word)
+        # BFS
+        queue = collections.deque([(beginWord, 1)])
+        visited = []
+        while queue:
+            cur_word, level = queue.popleft()
+            for i in range(l):
+                look_up_word = cur_word[:i] + "*" + cur_word[i+1:]
+                for word in new_dict[look_up_word]:
+                    if word == endWord:
+                        return level + 1
+                    if word not in visited:
+                        visited.append(word)
+                        queue.append((word, level+1))
+                new_dict[look_up_word] = []
+        return 0
+        
