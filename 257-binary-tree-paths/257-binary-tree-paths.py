615c9e301b1732d34d87f5971685e1ba92247688
lililin629 <42260704+lililin629@users.noreply.github.com>
2022-07-20 09:47:08 +0800

    Time: 37 ms (85.42%), Space: 14 MB (28.63%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.py
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -5,24 +5,45 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        path, paths = [root], []
-        self.find_path(root, path, paths)
-        return paths
+#     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+#         path, paths = [root], []
+#         self.find_path(root, path, paths)
+#         return paths
         
     
-    def find_path(self, node, path, paths):
-        if not node:
-            return
-        if not node.left and not node.right:
-            paths.append("->".join([str(n.val) for n in path]))
-            return
+#     def find_path(self, node, path, paths):
+#         if not node:
+#             return
+#         if not node.left and not node.right:
+#             paths.append("->".join([str(n.val) for n in path]))
+#             return
         
-        path.append(node.left)
-        self.find_path(node.left, path, paths)
-        path.pop()
+#         path.append(node.left)
+#         self.find_path(node.left, path, paths)
+#         path.pop()
         
-        path.append(node.right)
-        self.find_path(node.right, path, paths)
-        path.pop()
+#         path.append(node.right)
+#         self.find_path(node.right, path, paths)
+#         path.pop()
+
+
+# divide and conquer
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        paths = []
+        # exception
+        if not root:
+            return paths
+        # base case
+        if not root.left and not root.right:
+            return [str(root.val)]
+        # divide and concquer
+        for path in self.binaryTreePaths(root.left):
+            paths.append(str(root.val) + '->' + path)
+        for path in self.binaryTreePaths(root.right):
+            paths.append(str(root.val) + '->' + path)
+        return paths
+        
+        
+        
+        
         
