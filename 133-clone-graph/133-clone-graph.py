ef629d74a03c9b2fb43ebb933b2c1c2693f57290
lililin629 <42260704+lililin629@users.noreply.github.com>
2022-07-22 16:03:49 +0800

    Time: 89 ms (5.02%), Space: 14.5 MB (26.20%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.py
@@ -0,0 +1,49 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        # Find all nodes
+        nodes = self.find_nodes(node)
+        # Map all nodes
+        mapping = self.map_nodes(nodes)
+        # Map all edges
+        self.map_edges(mapping)
+        return mapping[node]
+        
+        
+    def find_nodes(self, node):
+        queue = collections.deque([node])
+        visited = set([node])
+        while queue:
+            cur_node = queue.popleft()
+            for neighbor in cur_node.neighbors:
+                if neighbor in visited:
+                    continue
+                queue.append(neighbor)
+                visited.add(neighbor)
+        return visited
+    
+    
+    def map_nodes(self, nodes):
+        mapping = {}
+        for node in nodes:
+            mapping[node] = Node(node.val)
+        return mapping
+    
+    
+    def map_edges(self, mapping):
+        for node in mapping:
+            new_node = mapping[node]
+            for neighbor in node.neighbors:
+                new_neighbor = mapping[neighbor]
+                new_node.neighbors.append(new_neighbor)
+            
+            
