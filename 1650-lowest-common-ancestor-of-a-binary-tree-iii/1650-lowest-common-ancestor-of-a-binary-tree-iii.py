59e4415db1b7d2deb115795cc9387bfa468940bf
lililin629 <42260704+lililin629@users.noreply.github.com>
2022-07-29 10:37:48 +0800

    Time: 73 ms (91.62%), Space: 18.4 MB (33.86%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -8,20 +8,49 @@ class Node:
         self.parent = None
 """
 
+# sol1: set
+# class Solution:
+#     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+#         if not q or not p:
+#             return None
+#         parent_set = set()
+#         cur = p
+#         while cur is not None:
+#             parent_set.add(cur)
+#             cur = cur.parent
+        
+#         cur = q
+#         while cur is not None:
+#             if cur in parent_set:
+#                 return cur
+#             cur = cur.parent
+
+
+# sol2: list
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         if not q or not p:
             return None
-        parent_set = set()
-        cur = p
-        while cur is not None:
-            parent_set.add(cur)
-            cur = cur.parent
+        list_p = self.parent_list(p)
+        list_q = self.parent_list(q)
         
-        cur = q
+        lca = None
+        while len(list_p) > 0 and len(list_q) > 0:
+            a = list_p.pop()
+            b = list_q.pop()
+            if a == b:
+                lca = a
+        return lca
+    
+    
+    def parent_list(self, node):
+        list_node = []
+        cur = node
         while cur is not None:
-            if cur in parent_set:
-                return cur
+            list_node.append(cur)
             cur = cur.parent
+        return list_node
+        
+        
         
         
