11219bd35343c65abb7036370cda3bb6d77509ca
lililin629 <42260704+lililin629@users.noreply.github.com>
2022-08-05 00:23:15 +0800

    Time: 52 ms (54.16%), Space: 14.1 MB (35.35%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = []
+        if not nums:
+            return subsets
+        self.dfs(sorted(nums), 0, [], subsets)
+        return subsets
+        
+    
+    def dfs(self, nums, index, subset, subsets):
+        if index == len(nums):
+            subsets.append(list(subset))
+            return
+        
+        # chosen
+        subset.append(nums[index])
+        self.dfs(nums, index+1, subset, subsets)
+        # not chosen
+        subset.pop()
+        self.dfs(nums, index+1, subset, subsets)
+        
+        
