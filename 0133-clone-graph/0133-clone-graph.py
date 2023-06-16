67ab1bb54a452bdd77bb626715fb62ace4aa32fe
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-06-16 09:34:39 -0400

    Time: 62 ms (10.80%), Space: 16.9 MB (19.84%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,44 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+    
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if node is None:
+            return None
+        if node.neighbors is None or node.neighbors == []:
+            return Node(node.val, None) 
+        
+        # created placeholder map val->new_nodes
+        queue = deque([node])
+        clones = {}
+        while queue:
+            og = queue.popleft()
+            for nei in og.neighbors:
+                if nei.val not in clones:
+                    clone = Node(nei.val, [])
+                    clones[nei.val] = clone
+                    queue.append(nei)
+                    
+                
+        # add neighbors to things in map
+        queue = deque([node])
+        visited = set([node.val])
+        while queue:
+            og = queue.popleft()
+            
+            for nei in og.neighbors:
+                clones[og.val].neighbors.append(clones[nei.val])
+                if nei.val not in visited:
+                    visited.add(nei.val)
+                    queue.append(nei)
+       
+        return clones[node.val]
+                    
+                    
+        
