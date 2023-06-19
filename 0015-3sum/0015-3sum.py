8eef15d4f802300a4e53484deb3c8b5c7bfd3356
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-06-19 08:12:09 -0400

    Time: 2353 ms (31.63%), Space: 21.7 MB (5.82%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,59 +1,32 @@
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         nums.sort()
-#         ans = []
-        
-#         for i in range(len(nums)):
-#             if nums[i] > 0:
-#                 break
-#             if  i != 0 and nums[i - 1] == nums[i]:
-#                 continue
-            
-            
-#             target = -nums[i]
-#             f = i+1
-#             e = len(nums)-1
-#             while f < e:
-#                 if nums[f]+nums[e] == target:
-#                     ans.append([nums[i], nums[f], nums[e]])
-#                     f += 1
-#                     e -= 1
-#                     while f < e and nums[f] == nums[f-1]:
-#                         f += 1
-                    
-#                 if nums[f]+nums[e] < target:
-#                     f += 1
-#                 else:
-#                     e -= 1
-#         return ans
-                    
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
+        ans = []
+        ans_set = set()
+        
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
-        return res
+            
+            
+            target = -nums[i]
+            f = i+1
+            e = len(nums)-1
+            while f < e:
+                if nums[f]+nums[e] == target:
+                    new_ans = (nums[i], nums[f], nums[e])
+                    if new_ans not in ans_set:
+                        ans_set.add(new_ans)
+                        ans.append([nums[i], nums[f], nums[e]])
+                    f += 1
+                    e -= 1
+                    continue
 
-    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]):
-        lo, hi = i + 1, len(nums) - 1
-        while (lo < hi):
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
-                lo += 1
-            elif sum > 0:
-                hi -= 1
-            else:
-                res.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
-                    lo += 1            
+                if nums[f]+nums[e] < target:
+                    f += 1
+                    continue
+                else:
+                    e -= 1
                     
-            
-            
-            
-        
+        return ans
+                    
