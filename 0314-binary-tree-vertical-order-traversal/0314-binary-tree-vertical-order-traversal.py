b5cb7743ba568b6387f26266e63e5e6874915c7a
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-06-19 08:04:58 -0400

    Time: 56 ms (20.95%), Space: 16.4 MB (49.07%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -11,24 +11,18 @@ class Solution:
         dic = {root: 0} # {node: node.x}
         xdic = {} #{x:[node.val, ...]}
        
-        
         queue = deque([root])
-        
-        
-        # depth = 0
+           
         while queue:
-            # depth += 1
             cur_level = len(queue)
             for i in range(cur_level):
                 cur = queue.popleft()
-                # dic[cur][1] = depth
                 cur_x = dic[cur]
                 if cur_x not in xdic:
                     xdic[cur_x] = [cur.val]
                 else:
                     xdic[cur_x].append(cur.val)
                     
-                
                 if cur.left:
                     queue.append(cur.left)
                     dic[cur.left] = cur_x - 1
@@ -44,24 +38,7 @@ class Solution:
             
             
                     
-#         new_dic = {} #{x:[node.val, ...]}
-#         for node in dic:
-#             x = dic[node]
-           
-#             if x not in new_dic:
-#                 new_dic[x] = [(depth, node.val)]
-#             else:
-#                 new_dic[x].append((depth, node.val))
-            
-#         sorted_dict = dict(sorted(new_dic.items()))
-#         ans = []
-#         for key in sorted_dict:
-#             sorted_dict[key].sort()
-#             lst = []
-#             for item in sorted_dict[key]:
-#                 lst.append(item[1])
-#             ans.append(lst)
-#         return ans
+
         
             
                 
