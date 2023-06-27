4d797a942ac7f782b17e9f12ac863ec53ab920b3
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-06-26 17:14:28 -0700

    Time: 48 ms (84.23%), Space: 16.7 MB (22.80%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,17 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         ans_set = set()
+        ans = []
 
         if not nums:
             return [[]]
         
-        ans = []
-        
         for x in self.subsetsWithDup(nums[1:]):
-            x_t = tuple(sorted(x))
-            if x_t not in ans_set:
-                ans_set.add(x_t)
+            #不選
+            t = tuple(sorted(x))
+            if t not in ans_set:
+                ans_set.add(t)
                 ans.append(x)
+                
+            #選nums[0]    
             x1 = list(x)
             x1.append(nums[0])
             t1 = tuple(sorted(x1))
