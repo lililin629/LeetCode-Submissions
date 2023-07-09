8b801c62e9517079553b716691b8b10db98636b1
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-08 21:01:23 -0700

    Time: 212 ms (71.60%), Space: 20.1 MB (88.26%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -1,21 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        d = {}
-        if not root:
-            return 0
-        self.dfs(root, 0, d)
-        return sum(d[max(d)])
-        
-    def dfs(self, node, depth, d):
-        if not node:
-            return 
-        if depth not in d:
-            d[depth] = [node.val]
+        if not root.left and not root.right:
+            return root.val
         else:
-            d[depth].append(node.val)
+            left, d1 = self.dfs(root.left)
+            right, d2 = self.dfs(root.right)
+            if d1 == d2:
+                return left + right
+            else:
+                if d1 > d2:
+                    return left
+                return right
         
-        if node.left:
-            self.dfs(node.left, depth + 1, d)
-        if node.right:
-            self.dfs(node.right, depth + 1, d)
+    def dfs(self, root):
+        if not root:
+            return 0, 0
+        if not root.left and not root.right:
+            return root.val, 1
+        else:
+            left, d1 = self.dfs(root.left)
+            right, d2 = self.dfs(root.right)
+            if d1 == d2:
+                return left + right, d1+1
+            else:
+                if d1 > d2:
+                    return left, d1+1
+                return right, d2+1
+            
+            
+            
+        
+        
+                
+            
+            
         
