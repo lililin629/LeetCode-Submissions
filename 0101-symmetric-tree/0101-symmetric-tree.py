11beecc7464810ca5bc8c0079d0ad150709cf953
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-11 17:13:45 -0700

    Time: 63 ms (5.01%), Space: 16.5 MB (47.18%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # BST by level
+        # get a list for each level
+        # check if the list is palindromic
+        
+        q = deque([root])
+        
+        
+        while q:
+            ls = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                
+                if node.left:
+                    q.append(node.left)
+                    ls.append(node.left.val)
+                else:
+                    ls.append(-101)
+                
+                if node.right:
+                    q.append(node.right)
+                    ls.append(node.right.val)
+                else:
+                    ls.append(-101)
+               
+                
+            if not self.isPalin(ls):
+                return False
+        return True
+    
+    def isPalin(self, ls):
+        p1 = 0
+        p2 = len(ls)-1
+        while p1 < p2:
+            if ls[p1] != ls[p2]:
+                return False
+            p1 += 1
+            p2 -= 1
+        return True
+                
+        
