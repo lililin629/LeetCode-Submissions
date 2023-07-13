cb1fc92c1fd3dbad9f37ef80c664e0b03d1f71a6
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-12 18:29:29 -0700

    Time: 144 ms (5.02%), Space: 16.8 MB (27.19%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,39 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        
+        def is_valid(s):
+            stack = []
+            if len(s)%2 == 1:
+                return False
+            for ch in s:
+                if ch == ')':
+                    if stack:
+                        if stack.pop() == '(':
+                            continue
+                        else:
+                            return False
+                stack.append(ch)
+            if not stack:
+                return True
+            return False
+                
+            
+            
+        def helper(s):
+            
+            if len(s) == 2*n:
+                if is_valid(s):
+                    ans.append(s)
+                return 
+                
+            helper(s+'(')
+            helper(s+')')
+        
+        helper('')    
+        return ans
+    
+    
+        
+            
+        
