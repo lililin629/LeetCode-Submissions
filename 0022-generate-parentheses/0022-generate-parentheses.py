5775c31502b4e3856b09b2f7c723a722720a0826
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-14 08:54:33 -0700

    Time: 52 ms (49.21%), Space: 16.7 MB (27.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,37 +1,52 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
+####################################dfs##########################################
         ans = []
+        def dfs(cur_s, left, right):
+            if len(cur_s) == 2*n:
+                ans.append(cur_s)
+            if left < n:
+                dfs(cur_s+'(', left+1, right)
+            if right < left:
+                dfs(cur_s+')', left, right+1)
         
-        def is_valid(s):
-            stack = []
-            if len(s)%2 == 1:
-                return False
-            for ch in s:
-                if ch == ')':
-                    if stack:
-                        if stack.pop() == '(':
-                            continue
-                        else:
-                            return False
-                stack.append(ch)
-            if not stack:
-                return True
-            return False
-                
-            
-        def helper(s):
-            
-            if len(s) == 2*n:
-                if is_valid(s):
-                    ans.append(s)
-                return 
-                
-            helper(s+'(')
-            helper(s+')')
-        
-        helper('')    
+        dfs('', 0, 0)
         return ans
+
+        
+###################################brute force###################################
+#         ans = []
+        
+#         def is_valid(s):
+#             stack = []
+#             if len(s)%2 == 1:
+#                 return False
+#             for ch in s:
+#                 if ch == ')':
+#                     if stack:
+#                         if stack.pop() == '(':
+#                             continue
+#                         else:
+#                             return False
+#                 stack.append(ch)
+#             if not stack:
+#                 return True
+#             return False
+                
+            
+#         def helper(s):
+            
+#             if len(s) == 2*n:
+#                 if is_valid(s):
+#                     ans.append(s)
+#                 return 
+                
+#             helper(s+'(')
+#             helper(s+')')
+        
+#         helper('')    
+#         return ans
 
     
     
