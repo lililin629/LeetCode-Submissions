48d8391811b2fddab5bbbde7e8a99105744d5494
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-17 08:40:50 -0700

    Time: 184 ms (52.85%), Space: 26 MB (75.45%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,16 +1,37 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        else:
-            f = 0
-            s = 1
-            while s < len(nums):
-                if nums[f] == nums[s]:
-                    f += 2
-                    s += 2
+        b = 0
+        e = len(nums)-1
+        while b < e:
+            m = (b+e)//2
+            if nums[m] == nums[m-1]:
+                if (m-1-b) % 2 == 0:
+                    b = m + 1
                 else:
-                    return nums[f]
-            return nums[f]
+                    e = m - 2
+            elif nums[m] == nums[m+1]:
+                if (m-b) % 2 == 0:
+                    b = m + 2
+                else:
+                    e = m - 1
+            else:
+                return nums[m]
+        return nums[b]
+            
+        
+        
+####################################Brute Force######################################
+#         if len(nums) == 1:
+#             return nums[0]
+#         else:
+#             f = 0
+#             s = 1
+#             while s < len(nums):
+#                 if nums[f] == nums[s]:
+#                     f += 2
+#                     s += 2
+#                 else:
+#                     return nums[f]
+#             return nums[f]
                 
         
