2b702e23f9f8d640bbb765181276a01a16a86607
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-17 17:17:39 -0700

    Time: 59 ms (5.26%), Space: 16.2 MB (98.74%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -7,28 +7,21 @@
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         # root, left, right
-        ls = [] #[[1,2], [1,3]]
-        def helper(root, cur_ls):
-            if not root:
-                return
-            cur_ls.append(root.val)
-            if not root.left and not root.right:
-                ls.append(list(cur_ls))
-                cur_ls.pop()
-                return
-            
-            helper(root.left, cur_ls)
-            helper(root.right, cur_ls)
-            cur_ls.pop()
-            
+        self.total = 0
+        def helper(root, cur_num):
+            if root:
+                cur_num = cur_num*10 + root.val
+                if not root.left and not root.right:
+                    self.total += cur_num
+                    return
+                helper(root.left, cur_num)
+                helper(root.right, cur_num)
+                
+
         
-        helper(root, [])
-        ans = 0
-        print(ls)
-        for l in ls:
-            for i in range(len(l)):
-                ans += l[i]*(10**(len(l)-1-i))
-        return ans
+        helper(root, 0)
+        
+        return self.total
             
             
             
