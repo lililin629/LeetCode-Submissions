adcbc7d6c43f0f2e305492f4bd3bd5c584813754
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-18 15:06:26 -0700

    Time: 330 ms (69.93%), Space: 18.6 MB (95.90%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,41 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        
+        def bfs(i, j):
+            dir = [(1, 0),(0, 1),(-1, 0),(0, -1)]
+            q = deque([(i, j)])
+            while q:
+                cur_x, cur_y = q.popleft()
+                for dx, dy in dir:
+                    x, y = cur_x + dx, cur_y + dy
+                    if x >= 0 and y >= 0 and x < len(grid) and y < len(grid[0]):
+                        if grid[x][y] == '1':
+                            grid[x][y] = '0'
+                            q.append((x, y))
+                                
+
         count = 0
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
                     count += 1
-                    self.bfs(i, j, grid) # change connected grids to 2
+                    bfs(i, j)
         return count
-                    
-        
-    def bfs(self, i, j, grid):
-        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        queue = deque([(i, j)])
-        
-        grid[i][j] = "2"
-        
-        while queue:
-            cur_x, cur_y = queue.popleft()
-            for dx, dy in dirs:
-                nx, ny = cur_x + dx, cur_y + dy
-                if nx >= 0 and ny >= 0 and nx < len(grid) and ny < len(grid[0]):
-                    if grid[nx][ny] == "1":
-                        grid[nx][ny] = "2"
-                        queue.append((nx, ny))
-                    
-                
         
         
-                    
-                            
-                        
-                    
-                
-                
         
