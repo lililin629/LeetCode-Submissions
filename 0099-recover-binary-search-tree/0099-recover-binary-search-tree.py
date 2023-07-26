2a2c8d50446d4ba248b734beff4c4b2420bfcb05
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-26 18:37:32 -0400

    Time: 76 ms (92.48%), Space: 16.7 MB (49.24%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
@@ -10,43 +10,29 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-#         prev = None
-#         nodes = []
-        
-#         def find_nodes(prev, root):
-#             if not root:
-#                 return
-#             # left mid right
-#             find_nodes(prev, root.left)
-#             if prev.val > root.val:
-#                 nodes.append(root)
-#             prev = root
-#             find_nodes(prev, root.right)
-        
-#         find_nodes(prev, root)
-#         nodes[0].val, nodes[1].val = nodes[1].val, nodes[0].val
+       
             
   
         
-#         prev = TreeNode(float('-inf'))
-#         high = None
-#         low = None
+        prev = TreeNode(float('-inf'))
+        high = None
+        low = None
 
-#         def find_nodes(root):
-#             nonlocal prev, high, low
-#             if not root:
-#                 return
-#             find_nodes(root.left)
-#             if prev.val > root.val:
-#                 if not high:
-#                     high = prev
-#                 low = root
-#             prev = root
-#             find_nodes(root.right)
-#             return high, low
+        def find_nodes(root):
+            nonlocal prev, high, low
+            if not root:
+                return
+            find_nodes(root.left)
+            if prev.val > root.val:
+                if not high:
+                    high = prev
+                low = root
+            prev = root
+            find_nodes(root.right)
+            return high, low
         
-#         h, l = find_nodes(root)
-#         h.val, l.val = l.val, h.val
+        h, l = find_nodes(root)
+        h.val, l.val = l.val, h.val
            
 # class Solution:
 #     def recoverTree(self, root: Optional[TreeNode]) -> None:
@@ -55,33 +41,32 @@ class Solution:
         
 #         """
         
-        dummy = TreeNode(float('-inf'))
-        seq = [dummy]
-        high = None
-        low = None
+#         dummy = TreeNode(float('-inf'))
+#         seq = [dummy]
+#         high = None
+#         low = None
         
-        def find_nodes(root):
-            nonlocal high, low
-            # left, root, right
-            if not root:
-                return 
-            find_nodes(root.left)
-            if root.val < seq[-1].val:
-                if not high:
-                    high = seq[-1]
-                low = root
-            seq.append(root)
+#         def find_nodes(root):
+#             nonlocal high, low
+#             # left, root, right
+#             if not root:
+#                 return 
+#             find_nodes(root.left)
+#             if root.val < seq[-1].val:
+#                 if not high:
+#                     high = seq[-1]
+#                 low = root
+#             seq.append(root)
         
-            find_nodes(root.right)
+#             find_nodes(root.right)
 
 
-        def rotate_nodes(a, b):
-            temp = a.val
-            a.val = b.val
-            b.val = temp
+#         def rotate_nodes(a, b):
+#             temp = a.val
+#             a.val = b.val
+#             b.val = temp
         
-        find_nodes(root)
-        print(low, high)
-        rotate_nodes(low, high)
+#         find_nodes(root)
+#         rotate_nodes(low, high)
         
 
