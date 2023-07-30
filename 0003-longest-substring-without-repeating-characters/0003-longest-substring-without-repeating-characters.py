d505156e2651487291d315d149c8c8fd1dcdc764
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-07-30 08:53:35 -0400

    Time: 64 ms (92.51%), Space: 16.5 MB (49.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,17 +4,81 @@ class Solution:
             return 0
         l = 0
         r = 1
-        visited = {s[0]:1,}
-        count = 1
+        maxl = 1
+        visited = set([s[l]])
+       
+        
         while r < len(s):
-            if s[r] not in visited or visited[s[r]] == 0:
-                visited[s[r]] = 1
+            if s[r] not in visited:
+                visited.add(s[r])
+                maxl = max(maxl, r-l+1)
                 r += 1
-                count = max(count, r-l)
+                
             else:
-                visited[s[l]] -= 1
-                l += 1
-        return count
+                visited.remove(s[l])
+                l += 1  
+      
+            
+        return maxl
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if not s:
+        #     return 0
+        # l = 0
+        # r = 1
+        # visited = {s[0]:1,}
+        # count = 1
+        # while r < len(s):
+        #     if s[r] not in visited or visited[s[r]] == 0:
+        #         visited[s[r]] = 1
+        #         r += 1
+        #         count = max(count, r-l)
+        #     else:
+        #         visited[s[l]] -= 1
+        #         l += 1
+        # return count
         
                 
                 
