4c6e2429950e6eafc98f19a59e93063b3ab1434f
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-04 14:10:55 -0400

    Time: 137 ms (95.27%), Space: 17.7 MB (85.89%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,50 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+       
+        m = len(board)
+        n = len(board[0])
+        start = []
+        # find the 0's on the border
+        for i in [0, m-1]:
+            for j in range(n):
+                if board[i][j] == "O":
+                    start.append((i, j))
+        
+        for i in range(1, m-1):
+            for j in [0, n-1]:
+                if board[i][j] == 'O':
+                    start.append((i, j))
+        
+        self.bfs(start, m, n, board) # '1'
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                if board[i][j] == '1':
+                    board[i][j] = 'O'
+
+
+                    
+    def bfs(self, start, m, n, board):
+        q = deque(start)
+        visited = set()
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        while q:
+            x, y = q.popleft()
+            visited.add((x, y))
+            board[x][y] = '1'
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if nx >= 0 and ny >= 0 and nx < m and ny < n and (nx, ny) not in visited:
+                    if board[nx][ny] == 'O':
+                        q.append((nx, ny))
+
+
+
+
+
