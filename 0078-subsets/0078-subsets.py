deee193d8ef52f748ea97afbd2d97e4297709420
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-07 11:12:12 -0400

    Time: 34 ms (97.92%), Space: 16.5 MB (78.06%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,27 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # base case
-        if not nums:
-            return [[]]
-        # divide
-        ans = []
-        for lst in self.subsets(nums[1:]):
-            ans.append(lst)
-            ans.append(lst + [nums[0]])      # make a new lst, do not mutate lst
-        return ans
-            
+        n = len(nums)
+        output = [[]]
+        
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        
+        return output
+#         self.ans = []
+        
+#         for l in range(len(nums)+1):
+#             self.dfs(l, 0, nums, [])
+#         return self.ans
+        
+    
+#     def dfs(self, l, pointer, nums, cur):
+#         if len(cur) == l:
+#             self.ans.append(list(cur))
+#             return
+        
+#         for j in range(pointer, len(nums)):
+#             cur.append(nums[j])
+#             self.dfs(l, j+1, nums, cur)
+#             cur.pop()
+        
         
