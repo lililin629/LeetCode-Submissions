9289e3d43193d07871f4864282805bfc1df7b3b6
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-08 09:30:21 -0400

    Time: 173 ms (84.14%), Space: 21.9 MB (55.51%) - LeetHub

---
+++ 1245-tree-diameter/1245-tree-diameter.py
@@ -0,0 +1,52 @@
+class Solution:
+    def treeDiameter(self, edges: List[List[int]]) -> int:
+        n = len(edges) + 1
+        d = defaultdict(list)
+        
+        for edge in edges:
+            d[edge[0]].append(edge[1])
+            d[edge[1]].append(edge[0])
+           
+        self.diameter = 0
+        
+        # it's an undirected connected component, starting from anywhere is the same
+        root = 0
+        visited = set()
+        
+        # get the diameter of each node
+        self.dfs(root, d, visited)
+        return self.diameter
+    
+    
+    def dfs(self, root, d, visited):
+        long1, long2 = 0, 0
+        visited.add(root)
+        if not d[root]:
+            return 0
+        
+        for node in d[root]:
+            if node not in visited:
+                cur_dia = self.dfs(node, d, visited)+1
+                if cur_dia > long1:
+                    long2 = long1
+                    long1 = cur_dia
+                    continue
+                if cur_dia > long2:
+                    long2 = cur_dia
+        self.diameter = max(self.diameter, long1+long2)
+        return long1
+        
+        
+            
+            
+            
+            
+        
+        
+        
+        
+        
+            
+            
+        
+        
