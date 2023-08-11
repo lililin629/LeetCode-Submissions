a0c2166c0e0a0c6b3269aec45b7a6e7ea88ae5fe
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-11 09:41:19 -0400

    Time: 625 ms (93.10%), Space: 47.4 MB (80.54%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,24 +5,60 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+        # reverse the second half (O(1) space complexity)
+        # 快慢指針找到中點
         
-        ls = []
-        cur = head
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
         
+        # reverse second half
+        pre = None
+        cur = slow
         while cur:
-            ls.append(cur.val)
-            cur = cur.next
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        head2 = pre
         
-        l = 0
-        r = len(ls) - 1
+        # 2 pointers
+        p1 = head
+        p2 = head2
+        maxv = 0
+        while p1 and p2:
+            maxv = max(p1.val+p2.val, maxv)
+            p1 = p1.next
+            p2 = p2.next
         
-        max_ans = 0
-        while l < r:
-            max_ans = max(max_ans, ls[l]+ls[r])
-            l += 1
-            r -= 1
+        return maxv
         
         
-        return max_ans
+        
+            
+        
+        
+# class Solution:
+#     def pairSum(self, head: Optional[ListNode]) -> int:
+        
+#         ls = []
+#         cur = head
+        
+#         while cur:
+#             ls.append(cur.val)
+#             cur = cur.next
+        
+#         l = 0
+#         r = len(ls) - 1
+        
+#         max_ans = 0
+#         while l < r:
+#             max_ans = max(max_ans, ls[l]+ls[r])
+#             l += 1
+#             r -= 1
+        
+        
+#         return max_ans
             
         
