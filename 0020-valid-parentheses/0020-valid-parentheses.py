5f2aa6ca4f50e4d448287b46f97426c271e77541
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-17 07:23:51 -0400

    Time: 39 ms (79.39%), Space: 16.4 MB (52.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,33 +1,57 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        self.st = []
-        # FILO
+        st = []
+        dic = {')':'(', 
+               ']':'[',
+               '}':'{'}
         for ch in s:
-            if ch == '(' or ch == '{' or ch == '[':
-                self.st.append(ch)
-            if ch == ')':
-                if self.check(ch, '('):
-                    continue
-                return False
-            if ch == ']':
-                if self.check(ch, '['):
-                    continue
-                return False
-            if ch == '}':
-                if self.check(ch, '{'):
-                    continue
-                return False
-                
-            
-        if len(self.st) != 0:
-            return False
-        return True
-    
-    def check(self, ch, lp):
-        if self.st and self.st[-1] == lp:
-            self.st.pop()
+            if ch in dic:
+                if st and st[-1] == dic[ch]:
+                    st.pop()
+                else:
+                    return False
+            else:
+                st.append(ch)
+        if not st:
             return True
         return False
+                
+                
+        
+        
+        
+        
+        
+        
+        
+#         self.st = []
+#         # FILO
+#         for ch in s:
+#             if ch == '(' or ch == '{' or ch == '[':
+#                 self.st.append(ch)
+#             if ch == ')':
+#                 if self.check(ch, '('):
+#                     continue
+#                 return False
+#             if ch == ']':
+#                 if self.check(ch, '['):
+#                     continue
+#                 return False
+#             if ch == '}':
+#                 if self.check(ch, '{'):
+#                     continue
+#                 return False
+                
+            
+#         if len(self.st) != 0:
+#             return False
+#         return True
+    
+#     def check(self, ch, lp):
+#         if self.st and self.st[-1] == lp:
+#             self.st.pop()
+#             return True
+#         return False
         
             
                 
