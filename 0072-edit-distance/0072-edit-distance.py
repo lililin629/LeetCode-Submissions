bc5cfc4756263a82516acd7fec99070ef3b756c2
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-22 10:16:10 -0500

    Time: 77 ms (95.60%), Space: 19.2 MB (74.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -3,22 +3,30 @@ class Solution:
         
         l = len(word1)-1
         r = len(word2)-1
+        self.dp = [ [-1]*(r+1) for _ in range(l+1) ]
+        
         return self.helper(word1, word2, l, r)
         
         
-    @cache
+    
     def helper(self, word1, word2, l, r):
-        if l < 0:
-            return r + 1
-        if r < 0:
-            return l + 1
-            
-        if word1[l] == word2[r]:
-            return self.helper(word1, word2, l-1, r-1)
         
+        if l == -1:
+            return r+1
+        if r == -1:
+            return l+1
+        
+        if self.dp[l][r] != -1:
+            return self.dp[l][r]
+        
+        if word1[l] == word2[r]:
+            self.dp[l][r] = self.helper(word1, word2, l-1, r-1)
+            return self.dp[l][r]
+       
         
         d = 1 + self.helper(word1, word2, l, r-1)
         i = 1 + self.helper(word1, word2, l-1, r)
-        r = 1 + self.helper(word1, word2, l-1, r-1)
-        return min(d, i, r)
+        re = 1 + self.helper(word1, word2, l-1, r-1)
+        self.dp[l][r] = min(d, i, re)
+        return self.dp[l][r]
         
