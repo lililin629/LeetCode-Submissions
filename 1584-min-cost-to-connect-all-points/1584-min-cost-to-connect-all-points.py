b83cb632dca88ce1a178812fadaa99da7e02de56
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-28 11:56:32 -0400

    Time: 602 ms (98.06%), Space: 17 MB (95.42%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -3,30 +3,32 @@ class Solution:
         n = len(points)
         dist = [float('inf')]*n
         dist[0] = 0
-        
-        linked = set()
-        ans = 0
-        # heap = [(dist[0], 0)]
-        # heapq.heapify(heap)
-        
-        while len(linked) < n+1:
-            # cur = heapq.heappop(heap)
+        linked = set([0])
+       
+        self.update_dist(n, points, dist, points[0][0], points[0][1], linked)
+        while len(linked) < n:
             mind = float('inf')
             cur = -1
             for i, d in enumerate(dist):
                 if i not in linked and d < mind:
                     mind = d
                     cur = i
-                    ans += mind
             [curx, cury] = points[cur]
             linked.add(cur)
-            for i in range(n):
+            self.update_dist(n, points, dist, curx, cury,linked)
+            # for i in range(n):
+            #     if i not in linked:
+            #         [xi, yi] = points[i]
+            #         dist[i] = min(dist[i], abs(curx-xi)+abs(cury-yi))
+        print(dist)            
+        return sum(dist)
+    
+    def update_dist(self, n, points, dist, curx, cury, linked):
+        for i in range(n):
                 if i not in linked:
                     [xi, yi] = points[i]
                     dist[i] = min(dist[i], abs(curx-xi)+abs(cury-yi))
-                    # heapq.heappush(heap, (dist[i], i))
-        print(dist)
-        return sum(dist)
+        
                     
             
         
