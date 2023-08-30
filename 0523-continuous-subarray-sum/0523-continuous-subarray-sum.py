47cf3bb6cf978a2c08b2de5c9e68ac280515dfbe
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-08-30 11:08:14 -0400

    Time: 943 ms (56.10%), Space: 36.2 MB (48.18%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,26 +1,19 @@
+# (Sum - prev_sum)%k = ck%k
+# sum%k - prev_sum%k = 0
+# sum%k = prev_sum%k
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        for i in range(1, len(nums)):
-            if nums[i-1]%k == 0 and nums[i]%k == 0:
-                return True
-        s = set()
-        s.add(0)
-        prefix_sum = 0
-        ps1 = 0
+        d = defaultdict(int)
+        d[0] = -1
+        s = 0
         
         for i in range(len(nums)):
-            prefix_sum += nums[i]
-            ps1 = prefix_sum % k
             
-            
-            if nums[i] % k == 0:
-                continue
-                
-          
-            if ps1 in s:
+            s += nums[i]
+            if s % k not in d:
+                d[s%k] = i
+            elif d[s%k] < i-1:
                 return True
-            s.add(ps1)
-            
         return False
             
             
