27e86218d6f380e46e46d719c2637b4ebdb5417e
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-01 15:36:54 -0400

    Time: 38 ms (67.81%), Space: 16.4 MB (37.32%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,38 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        st = []
+        
+        for ch in s:
+    
+            decoded = ''
+            if ch == ']':
+                while st[-1] != '[':
+                    decoded += st.pop()
+                st.pop()
+                num = ''
+                while st and st[-1].isnumeric():
+                    
+                    num = st.pop() + num
+                print(num)
+                num1 = 0
+                for n in num:
+                    num1 = num1*10 + int(n)
+                    
+                for _ in range(num1):
+                    for c in decoded[::-1]:
+                        st.append(c)
+                print(st)
+                    
+            else:
+                # push into stack in order
+                st.append(ch)
+        ans = ''
+    
+        while st:
+            ans = st.pop() + ans
+        return ans
+            
+       
+                
+            
+        
