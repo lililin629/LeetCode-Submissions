82d9898ddb12b6f7abff58b092c0c12f8777897d
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-01 21:52:08 -0400

    Time: 205 ms (81.31%), Space: 17.1 MB (82.30%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,39 @@
+class DSU:
+    def __init__(self):
+        self.par = [-1]*1001
+    def find(self, x):
+        if self.par[x] > 0:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+        return x
+    
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px == py:
+            return False
+        elif self.par[px] < self.par[py]:
+            self.par[py] = px
+            self.par[px] -= 1
+        else:
+            self.par[px] = py
+            self.par[py] -= 1
+        return True
+        
+    
+    
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        dsu = DSU()
+        n = len(isConnected)
+        for i in range(n):
+            for j in range(1, n-i):
+                if isConnected[i][i+j]:
+                    dsu.union(i, i+j)
+        ans = 0
+        for i in range(n):
+            if dsu.par[i] < 0:
+                ans += 1
+        return ans
+                    
+                    
