064428ae0dec4a13b9918ebc52b2a315f3a04b6d
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-05 18:32:56 -0400

    Time: 550 ms (77.87%), Space: 23.2 MB (20.49%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -5,20 +5,20 @@ class Solution:
             graph[x].append((cost, y))
             graph[y].append((cost, x))
         
-        heap = [(0, 1, 1)]
+        heap = [(0, 1)]
         heapq.heapify(heap)
         connected = set()
         total_cost = 0
         
         while heap:
             
-            cur_cost, curx, cury = heapq.heappop(heap)
+            cur_cost, cury = heapq.heappop(heap)
             if cury not in connected:
                 total_cost += cur_cost
                 connected.add(cury)
                 for (cost, ny) in graph[cury]:
                     if ny not in connected:
-                        heapq.heappush(heap,(cost, cury, ny))
+                        heapq.heappush(heap,(cost, ny))
             
         return total_cost if len(connected) == n else -1
             
