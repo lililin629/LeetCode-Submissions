5c1f96509199b8f166f3644a8da9a3f0af928866
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-06 17:54:44 -0400

    Time: 3123 ms (24.88%), Space: 20.5 MB (94.63%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -2,31 +2,64 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
         
-        dist = [self.calc_dist(0, i, points) for i in range(n)]
+        dist = [float('inf')]*n
+        dist[0] = 0
+        heap = []
+        for i, d in enumerate(dist):
+            heap.append((d, i))
+
         linked = set([0])
+        
        
-        while len(linked) < n:
-            mind = float('inf')
-            cur = -1
-            for i, d in enumerate(dist):
-                if i not in linked and d < mind:
-                    mind = d
-                    cur = i
-            [curx, cury] = points[cur]
+        while heap:
+            # print(heap)
+            cur_dist, cur = heapq.heappop(heap)
+            # print(f"{cur}, {cur_dist}")
+            
             linked.add(cur)
 
             for i in range(n):
                 if i not in linked:
-                    dist[i] = min(dist[i], self.calc_dist(cur, i, points))
-               
+                    if self.calc_dist(cur, i, points) < dist[i]:
+                        dist[i] = self.calc_dist(cur, i, points)
+                        heapq.heappush(heap, (dist[i], i))
+                        
         return sum(dist)
     
     def calc_dist(self, start, end, points):
         [sx, sy] = points[start]
         [ex, ey] = points[end]
         return abs(sx-ex) + abs(sy-ey)
-  
-                    
+
+    
+    
+# class Solution:
+#     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+#         n = len(points)
+        
+#         dist = [self.calc_dist(0, i, points) for i in range(n)]
+#         linked = set([0])
+       
+#         while len(linked) < n:
+#             mind = float('inf')
+#             cur = -1
+#             for i, d in enumerate(dist):
+#                 if i not in linked and d < mind:
+#                     mind = d
+#                     cur = i
+#             [curx, cury] = points[cur]
+#             linked.add(cur)
+
+#             for i in range(n):
+#                 if i not in linked:
+#                     dist[i] = min(dist[i], self.calc_dist(cur, i, points))
+               
+#         return sum(dist)
+    
+#     def calc_dist(self, start, end, points):
+#         [sx, sy] = points[start]
+#         [ex, ey] = points[end]
+#         return abs(sx-ex) + abs(sy-ey)                    
             
         
         
