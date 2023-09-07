2eacc6fec23ded6664483c1f3ba3fedf8e4353a3
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-06 21:30:23 -0400

    Time: 40 ms (55.40%), Space: 16.4 MB (27.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = {}
-        return self.dfs(n, dp)
-
-    def dfs(self, n, dp):
-        if n == 0 or n == 1:
+        return self.dp(n)
+        
+    
+    @cache
+    def dp(self, n):
+        if n == 1:
             return 1
-
+        if n == 2:
+            return 2
         else:
-            if n in dp:
-                return dp[n]
-            dp[n] = self.dfs(n-1, dp) + self.dfs(n-2, dp)
-            return dp[n]
+            return self.dp(n-2) + self.dp(n-1)
+        
         
