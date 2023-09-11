bdf1501f988b1c9aaa75bb747f97b797d141bb83
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-11 19:24:57 -0400

    Time: 73 ms (40.14%), Space: 16.5 MB (10.33%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,85 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:
-            return 0
-        l = 0
-        r = 1
-        maxl = 1
-        visited = set([s[l]])
-       
+        d = defaultdict(int)
+        
+        l = r = 0
+        ans = 0
         
         while r < len(s):
-            if s[r] not in visited:
-                visited.add(s[r])
-                maxl = max(maxl, r-l+1)
-                r += 1
-                
-            else:
-                visited.remove(s[l])
-                l += 1  
-      
+            d[s[r]] += 1
             
-        return maxl
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # if not s:
-        #     return 0
-        # l = 0
-        # r = 1
-        # visited = {s[0]:1,}
-        # count = 1
-        # while r < len(s):
-        #     if s[r] not in visited or visited[s[r]] == 0:
-        #         visited[s[r]] = 1
-        #         r += 1
-        #         count = max(count, r-l)
-        #     else:
-        #         visited[s[l]] -= 1
-        #         l += 1
-        # return count
-        
-                
-                
+            while d[s[r]] > 1:
+                d[s[l]] -= 1
+                l += 1
+            ans = max(ans, r-l+1)
+            r += 1
+        return ans
             
+                
