3631aa0cbb32997a7e8cfd337640d78e4d34c749
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-14 11:00:13 -0400

    Time: 1306 ms (79.22%), Space: 32.1 MB (93.88%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,36 +1,65 @@
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         # edge case
+#         if k == 1:
+#             return nums
+        
+#         # 2 pointers
+#         l = 0  #first ele of window
+#         r = k  #1 ele to the rihgt of the last ele
+#         cur_max = max(nums[:k])
+#         d = Counter(nums[:k])
+#         ans = [cur_max]
+        
+#         while r < len(nums):
+#             # update d 
+#             d[nums[r]] += 1
+#             d[nums[l]] -= 1
+#             if d[nums[l]] == 0:
+#                 del d[nums[l]]
+#             # update cur_max
+#             if nums[r] > cur_max:
+#                 cur_max = nums[r]
+#             else:
+#                 if cur_max not in d:
+#                     cur_max = max(d)
+#             ans.append(cur_max) 
+            
+#             # move window to the right
+#             l += 1
+#             r += 1
+        
+#         return ans        
+
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # edge case
         if k == 1:
             return nums
         
-        # 2 pointers
-        l = 0  #first ele of window
-        r = k  #1 ele to the rihgt of the last ele
-        cur_max = max(nums[:k])
-        d = Counter(nums[:k])
-        ans = [cur_max]
+        # Deque to store elements in decreasing order
+        dq = deque()
+        ans = []
         
-        while r < len(nums):
-            # update d 
-            d[nums[r]] += 1
-            d[nums[l]] -= 1
-            if d[nums[l]] == 0:
-                del d[nums[l]]
-            # update cur_max
-            if nums[r] > cur_max:
-                cur_max = nums[r]
-            else:
-                if cur_max not in d:
-                    cur_max = max(d)
-            ans.append(cur_max) 
+        for i, n in enumerate(nums):
+            # print(dq)
+            # Remove elements that are out of this window
+            if dq and i-dq[0]+1 > k:
+                dq.popleft()
             
-            # move window to the right
-            l += 1
-            r += 1
+            # Remove elements that are smaller than the current element
+            while dq and nums[dq[-1]] < n:
+                dq.pop()
+            
+            dq.append(i)
+           
+            # The first element is the maximum element of previous window, add it to the result
+            if i >= k - 1:
+                
+                ans.append(nums[dq[0]])
         
-        return ans        
-            
+        return ans
+
             
             
             
