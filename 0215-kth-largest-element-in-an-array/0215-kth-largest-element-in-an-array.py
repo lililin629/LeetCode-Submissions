b178091a02d938ed8a759997d5be307e92e38d4b
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-17 16:37:31 -0400

    Time: 520 ms (38.98%), Space: 29.6 MB (42.52%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,10 +1,38 @@
+#         nums2 = [-1*ele for ele in nums]
+#         heapq.heapify(nums2)
+        
+#         for _ in range(k):
+#             ans = heapq.heappop(nums2)
+            
+#         return -1*ans
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums2 = [-1*ele for ele in nums]
-        heapq.heapify(nums2)
+        n = len(nums)
+        return self.partition(0, n-1, n-k, nums) # n-k is the 0-based index of the kth largest number
         
-        for _ in range(k):
-            ans = heapq.heappop(nums2)
-            
-        return -1*ans
+    def partition(self, start, end, k, nums):
+        # partition
+        # invariant: start <= k <= end
+        if start == end:
+            return nums[k]
+        
+        l, r = start, end
+        mid = (l+r)//2
+        piv = nums[mid]
+        while l <= r:
+            while l <= r and nums[r] > piv:
+                r -= 1
+            while l <= r and nums[l] < piv:
+                l += 1
+            if l <= r:
+                nums[r],  nums[l] = nums[l], nums[r]
+                l += 1
+                r -= 1
+        
+        if r >= k:
+            return self.partition(start, r, k, nums)
+        if l <= k:
+            return self.partition(l, end, k, nums)
+        return nums[k]
+        
         
