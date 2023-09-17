661a192e32fe496edbb9bb21d19216541597b3ee
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-17 17:31:44 -0400

    Time: 91 ms (91.04%), Space: 21.2 MB (48.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,31 +3,33 @@ class Solution:
         d = defaultdict(int)
         for num in nums:
             d[num] += 1
-        d2 = defaultdict(list)
         
-        s = set()
-        for num, fre in d.items():
-            s.add(fre)
-            d2[fre].append(num)
+        new = [(count, num) for num, count in d.items()]
         
-        lst = list(s)
-        lst.sort(reverse = True)
+        n = len(d)
+        self.partition(0, n-1, new)
+        return [j for i, j in new[n-k:]]
+
+    def partition(self, start, end, new):
+        if start >= end:
+            return
+        l, r = start ,end
+        piv = new[(l+r)//2][0]
         
-        
-        ans = []
-        for i in range(len(lst)):
-            key = lst[i]
-            for num in d2[key]:
-                ans.append(num)
-                k -= 1
-            if k == 0:
-                break
-        return ans
+        while l <= r:
+            while l <= r and new[r][0] > piv:
+                r -= 1
+            while l <= r and new[l][0] < piv:
+                l += 1
             
+            if l <= r:
+                new[r], new[l] = new[l], new[r]
+                l += 1
+                r -= 1
+        self.partition(start, r, new)
         
+        self.partition(l, end, new)
         
-        
-        
-        
+                
+                
             
-        
