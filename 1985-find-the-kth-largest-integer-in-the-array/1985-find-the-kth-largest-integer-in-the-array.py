e0d0aa9e3db4a9c8e82ce058932fdb2fe747bd2e
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-18 11:49:58 -0400

    Time: 296 ms (24.45%), Space: 24.9 MB (85.37%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -1,9 +1,47 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        # n = len(nums)
+        # new = [int(num) for num in nums]
+        # heapq.heapify(new)
+        # for i in range(n-k+1):
+        #     cur = heapq.heappop(new)
+        # return str(cur)
+        ############################################################
+        #######################Quick Select#########################
+        
         n = len(nums)
         new = [int(num) for num in nums]
-        heapq.heapify(new)
-        for i in range(n-k+1):
-            cur = heapq.heappop(new)
-        return str(cur)
+        
+        return self.qs(0, n-1, new, n-k)
+        
+    def qs(self, start, end, new, k):
+        
+        if start == end:
+            return str(new[k])
+        l, r = start, end
+        piv = new[(start+end)//2]
+        
+        while l <= r:
+            while l <= r and new[r] > piv:
+                r -= 1
+            while l <= r and new[l] < piv:
+                l += 1
+            if l <= r:
+                new[r], new[l] =  new[l], new[r]
+                l += 1
+                r -= 1
+        
+        if k <= r:
+            self.qs(start, r, new, k)
+        if k >= l:
+            self.qs(l, end, new, k)
+        return str(new[k])
+            
+                
+        
+    
+        
+        
+        
+        
         
