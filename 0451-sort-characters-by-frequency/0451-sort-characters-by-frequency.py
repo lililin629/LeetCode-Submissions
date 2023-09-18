6945c7c46279a58d01da3139d0c9e79a9414f8ac
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-09-18 14:13:56 -0400

    Time: 65 ms (22.72%), Space: 17.5 MB (88.16%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,37 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        count = Counter(s)
+        lst = [(ct, ch) for (ch,ct) in count.items()]
+        
+        n = len(lst)
+        self.qs(0, n-1, lst)
+        ans = ''
+        for (ct, ch) in lst[::-1]:
+            for i in range(ct):
+                ans += ch
+        return ans
+    
+    def qs(self, start, end, lst):
+        if start >= end:
+            return
+        l, r = start, end
+        piv = lst[(l+r)//2]
+        while l <= r:
+            while l <= r and lst[r] > piv:
+                r -= 1
+            while l <= r and lst[l] < piv:
+                l += 1 
+            
+            if l <= r:
+                lst[l], lst[r] = lst[r], lst[l]
+                r -= 1
+                l += 1
+        self.qs(start, r, lst)
+        self.qs(l, end, lst)
+                
+            
+            
+        
+    
+        
+        
