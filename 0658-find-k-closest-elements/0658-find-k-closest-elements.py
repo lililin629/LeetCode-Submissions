eb5e372e64d568e509874ae9ce6e42b87124fac9
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-01 20:23:18 -0400

    Time: 332 ms (12.29%), Space: 18.8 MB (11.03%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -2,8 +2,6 @@ class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         diff_h = []
         heapq.heapify(diff_h)
-        ele_h = []
-        heapq.heapify(ele_h)
         for ele in arr:
             heapq.heappush(diff_h, (abs(ele-x), ele))
         ans = []
@@ -13,14 +11,7 @@ class Solution:
         ans.sort()
         return ans
             
-        # for _ in range(k):
-        #     dist, el = heapq.heappop(diff_h)
-        #     heapq.heappush(ele_h, el)
-        # ans = []
-        # for _ in range(k):
-        #     ans.append(heapq.heappop(ele_h))
-        # return ans
-            
+       
             
             
         
