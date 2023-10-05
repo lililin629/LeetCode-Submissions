a807184d34eef73c53f03eb1c25cf076dcf52954
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-04 21:58:06 -0400

    Time: 631 ms (20.37%), Space: 30.5 MB (32.90%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,28 +1,37 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        d = defaultdict(int)
-        ps = 0
-        d[-1] = ps
-        l = -1
-        r = 0
-        max_sum = float('-inf')
+        n = len(nums)
+        dp = [0]*n
+        dp[0] = nums[0]
+        for i in range(1,n):
+            dp[i] = max(dp[i-1]+nums[i], nums[i])
+        return max(dp)
         
-        while r < len(nums):
-            ps += nums[r]
-            d[r] = ps
+        
+##################################
+#         if len(nums) == 1:
+#             return nums[0]
+#         d = defaultdict(int)
+#         ps = 0
+#         d[-1] = ps
+#         l = -1
+#         r = 0
+#         max_sum = float('-inf')
+        
+#         while r < len(nums):
+#             ps += nums[r]
+#             d[r] = ps
             
-            if d[r]-d[l] >= max_sum:
-                max_sum = d[r]-d[l]
+#             if d[r]-d[l] >= max_sum:
+#                 max_sum = d[r]-d[l]
 
-            if ps < 0:
-                l = r
-                d[l] = 0
-                ps = 0
-            r += 1
+#             if ps < 0:
+#                 l = r
+#                 d[l] = 0
+#                 ps = 0
+#             r += 1
             
-        return max_sum
+#         return max_sum
             
             
         
