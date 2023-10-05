e3580fe7a5424317c15874506017113375da3c52
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-04 22:17:24 -0400

    Time: 43 ms (22.07%), Space: 16.2 MB (73.40%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,21 +1,30 @@
 class Solution:
-    def __init__(self):
-        self.d = {'2':['a','b','c'],'3':['d','e','f'],'4':['g','h','i'],
-             '5':['j','k','l'],'6':['m','n','o'],'7':['p','q','r','s'],
-             '8':['t','u','v'], '9':['w','x','y','z'] }
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        if len(digits) == 1:
-            return self.d[digits[0]]
-        ans = []
-        for s in self.letterCombinations(digits[1:]):
-            for ch in self.d[digits[0]]:
-                ans.append(ch+s)
-        return ans
-
-
-     
+        self.d = {
+            "2":["a","b","c"],
+            "3":["d","e","f"], 
+            "4":["g","h","i"], 
+            "5":["j","k","l"], 
+            "6":["m","n","o"], 
+            "7":["p","q","r","s"], 
+            "8":["t","u","v"], 
+            "9":["w","x","y","z"]
+            }
+        combos = []
+        self.dfs(digits, 0, '', combos)
+        return combos
+    
+    def dfs(self, digits, idx, combo, combos):
         
-
+        if idx == len(digits):
+            combos.append(combo)
+            return
+        
+        for ch in self.d[digits[idx]]:
+            self.dfs(digits, idx+1, combo+ch, combos)
+            
+        
+        
         
