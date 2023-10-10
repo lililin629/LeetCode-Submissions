ca2783f4a39ceee12fb2ff1c60a0aa4964e1a96b
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-10 09:43:14 -0400

    Time: 8688 ms (7.07%), Space: 16.2 MB (84.20%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,57 +1,34 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # iterate through the board to find the starting char
-        # dfs(start char)
-        # stops when len(word) is rached or when duplicate char is used
-        # check boundaries
-       
-        
-#         def bfs(row, col, s, visited):
-#             q = deque((row, col))
-#             visited.add((row, col))
-#             dirs = [(0,1), (0,-1), (1,0), (-1,0)]
-            
-#             lev = 0
-#             while q:
-#                 lev += 1
-#                 for i in range(len(q)):
-#                     curx, cury = q.popleft()
-#                     s += board[curx][cury]
-#                     if s != word[:lev]:
-                        
-#                         continue
-#                     for dx, dy in dirs:
-#                         nx, ny = curx+dx, cury+dy
-#                         if vlid(nx, ny):
-#                             q.add((nx, ny))
-#                             visited.add((nx, ny))
-  
-        def found(used, word, x, y):
-            if len(used) == len(word):
-                return True
-            dirs = [(0,1), (0,-1), (1,0), (-1,0)]
-            f = False
-            for dx, dy in dirs:
-                nx, ny = x+dx, y+dy
-                if valid(nx, ny) and word[len(used)] == board[nx][ny] and (nx, ny) not in used:
-                    used.add((nx,ny))
-                    f = f or found(used, word, nx, ny)
-                    used.remove((nx,ny))
-            return f        
-                
-        def valid(x, y):
-            if x < len(board) and x >= 0 and y < len(board[0]) and y >= 0:
-                return True
+        self.row = len(board)
+        self.col = len(board[0])
+        self.board = board
+        for i in range(self.row):
+            for j in range(self.col):
+                if self.backtrack(i, j, word):
+                    return True
+    
+    def backtrack(self, row, col, word):
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        if not len(word):
+            return True
+        if not self.valid(row, col, word):
             return False
         
-        
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                if board[row][col] == word[0] and found({(row,col)}, word, row, col):
-                    return True
+        self.board[row][col] = '-1'
+        for dx, dy in dirs:
+            nr, nc = row+dx, col+ dy
+            if self.backtrack(nr, nc, word[1:]):
+                return True
+        self.board[row][col] = word[0]
         return False
-#                 if board[row][col] == word[0]:
-#                     visited = {(row, col)}
-#                     bfs(row, col,'', visited)
-       
+    
+    def valid(self, r, c, word):
+        if r >= self.row or r < 0 or c >= self.col or c < 0 or self.board[r][c] != word[0]:
+            return False
+        return True
+                    
+            
+            
+        
         
