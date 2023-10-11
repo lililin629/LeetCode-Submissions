a8f26a0c000155aed6871cef083f1f23ece10fdc
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-11 16:32:51 -0400

    Time: 47 ms (6.31%), Space: 16.2 MB (54.88%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,16 +1,33 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1]
-        if rowIndex == 1:
-            return [1, 1]
-        ans = []
-        pre = self.getRow(rowIndex-1)
+        # if rowIndex == 0:
+        #     return [1]
+        # if rowIndex == 1:
+        #     return [1, 1]
+        # ans = []
+        # pre = self.getRow(rowIndex-1)
+        # for i in range(rowIndex+1):
+        #     if i == 0 or i == rowIndex:
+        #         ans.append(1)
+        #     else:
+        #         ans.append(pre[i-1] + pre[i])
+        # return ans
+        
+    
+    
+        d = defaultdict(int)
         for i in range(rowIndex+1):
-            if i == 0 or i == rowIndex:
-                ans.append(1)
-            else:
-                ans.append(pre[i-1] + pre[i])
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    d[(i,j)] = 1
+                else:
+                    d[(i,j)] = d[(i-1, j-1)] + d[(i-1, j)]
+        ans = []
+        for i in range(rowIndex+1):
+            ans.append(d[(rowIndex, i)])
         return ans
+            
+        
+        
             
         
