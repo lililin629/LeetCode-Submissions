18269b30228e005adf8270c8f0c0a5129f563a6c
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-15 14:25:17 -0400

    Time: 878 ms, Memory: 64.8 MB - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,17 +1,21 @@
 class Solution:
+    
     def combine(self, n: int, k: int) -> List[List[int]]:
         answers = []
         self.dfs(1, n, k, [], answers)
         return answers
     
     def dfs(self, start, n, k, ans, answers):
+        # if start > n:
+        #     return
         if len(ans) == k:
-            answers.append(sorted(list(ans)))
+            answers.append(list(ans))
             return
         
         for i in range(start, n+1):
             ans.append(i)
             self.dfs(i+1, n, k, ans, answers)
             ans.pop()
+        
         
         
