516952b2998cb508df94d73df2ea72147d7a2870
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-22 12:46:18 -0400

    Time: 666 ms (5.02%), Space: 16.4 MB (54.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,32 +1,61 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-            
-        # binary search for length
-        lo, hi = 1, len(s)
-        while lo+1 < hi:
-            mid = (lo + hi)//2
-            if self.isValid(s, mid, k):
-                lo = mid
-            else:
-                hi = mid
-        if self.isValid(s, hi, k):
-            return hi
-        return lo
+        all_char = Counter(s)
+        self.mx_len = 0
         
-    def isValid(self, s, length, k):
-        # sliding window
+        for ch in all_char:
+            self.find_len(ch, s, k)
+            
+        return self.mx_len
+    
+    def find_len(self, ch, s, k):
+        l = r = 0
         d = defaultdict(int)
-        max_ct = 0
-        start = 0
-        for end in range(len(s)):
-            d[s[end]] += 1
-            if end + 1 - start > length:
-                d[s[start]] -= 1
-                start += 1
-            max_ct = max(max_ct, d[s[end]])
-            if length - max_ct <= k:
-                return True
-        return False
+        while r < len(s):
+            d[s[r]] += 1
+            while r-l+1-d[ch] > k:
+                d[s[l]] -= 1
+                l += 1
+              
+            self.mx_len = max(self.mx_len, r-l+1)
+            r += 1
+           
+          
+                
+                
+            
+        
+        
+        
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+            
+#         # binary search for length
+#         lo, hi = 1, len(s)
+#         while lo+1 < hi:
+#             mid = (lo + hi)//2
+#             if self.isValid(s, mid, k):
+#                 lo = mid
+#             else:
+#                 hi = mid
+#         if self.isValid(s, hi, k):
+#             return hi
+#         return lo
+        
+#     def isValid(self, s, length, k):
+#         # sliding window
+#         d = defaultdict(int)
+#         max_ct = 0
+#         start = 0
+#         for end in range(len(s)):
+#             d[s[end]] += 1
+#             if end + 1 - start > length:
+#                 d[s[start]] -= 1
+#                 start += 1
+#             max_ct = max(max_ct, d[s[end]])
+#             if length - max_ct <= k:
+#                 return True
+#         return False
         
         
         
