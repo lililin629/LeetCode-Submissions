0ba15a4952967ca9419eba1637c7639ae5757813
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-29 16:42:36 -0400

    Time: 601 ms (36.04%), Space: 23.9 MB (56.80%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,39 +1,62 @@
-# class Solution:
-#     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-#         ans = []
-#         for i in range(len(graph)):
-#             for node in graph[i]:
-#                 if not self.safe(graph, node):
-#                     break
-#             ans.append(i)
-#         return ans
-        
-#     def safe(self, graph, node):
-#         if len(graph[node]) == 0:
-#             return True
-#         for child in graph[node]:
-#             if not self.safe(graph, child):
-#                 return False
-#         return True
-        
 class Solution:
-    def eventualSafeNodes(self, graph) -> List[int]:
-        def isSafe(node):
-            if state[node] != 0:
-                return state[node] == 2
-            state[node] = 1
-            for neighbor in graph[node]:
-                if not isSafe(neighbor):
-                    return False
-            state[node] = 2
-            return True
-
-        state = [0] * len(graph)  # 0: unvisited, 1: visiting, 2: safe
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         ans = []
+        self.safe_d = defaultdict(int)
+        self.graph = graph
         for i in range(len(graph)):
-            if isSafe(i):
+            if self.safe(i):
                 ans.append(i)
         return ans
+    def safe(self, node):
+        if self.safe_d[node] == 1:
+            return True
+        if self.safe_d[node] == -1:
+            return False
+        
+        self.safe_d[node] = -1
+        for child in self.graph[node]:
+            if not self.safe(child):
+                return False
+        self.safe_d[node] = 1
+        return True
+     
+#     def safe(self, node):
+#         if not self.graph[node]:
+#             self.safe_d[node] = 1
+#             return True
+#         for child in self.graph[node]:
+#             if child == node:
+#                 self.safe_d[node] = 0
+#                 return False
+#             if child in self.safe_d and not self.safe_d[child]:
+#                 return False
+#             if child not in self.safe_d:
+#                 if self.safe(child):
+#                     self.safe_d[child] = 1
+#                 else:
+#                     self.safe_d[child] = 0
+#                     return False
+                    
+#         return True
+        
+# class Solution:
+#     def eventualSafeNodes(self, graph) -> List[int]:
+#         def isSafe(node):
+#             if state[node] != 0:
+#                 return state[node] == 2
+#             state[node] = 1
+#             for neighbor in graph[node]:
+#                 if not isSafe(neighbor):
+#                     return False
+#             state[node] = 2
+#             return True
+
+#         state = [0] * len(graph)  # 0: unvisited, 1: visiting, 2: safe
+#         ans = []
+#         for i in range(len(graph)):
+#             if isSafe(i):
+#                 ans.append(i)
+#         return ans
             
                     
         
