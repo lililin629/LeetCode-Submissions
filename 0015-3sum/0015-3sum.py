33f7d8a95ce1b9a56a5f89cf4605bc7da99f59fa
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-30 13:48:54 -0400

    Time: 2811 ms (17.31%), Space: 21.6 MB (6.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,46 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans = []
-        ans_set = set()
-        
+        seen = set()
+        ans = set()
         for i in range(len(nums)):
-            if nums[i] > 0:
-                break
+            num = nums[i]
+            if num in seen:
+                continue
+            seen.add(num)
+            ls = self.twoSum(i, nums, -num)
+            for l in ls:
+                a = list(l)
+                a.append(num)
+                a.sort()
+                ans.add(tuple(a))
+        ret = []
+        for an in ans:
+            b = list(an)
+            ret.append(b)
+        return ret
+    
+    def twoSum(self, i, nums, target):
+        l = 0
+        r = len(nums)-1
+        s = set()
+        while l < r:
+            if l == i:
+                l += 1
+                continue
+            if r == i:
+                r -= 1
+                continue
+            if nums[l] + nums[r] == target:
+                s.add((nums[l], nums[r]))
+                r -= 1
+                l += 1
+            elif nums[l] + nums[r] > target:
+                r -= 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+        return s
             
             
-            target = -nums[i]
-            f = i+1
-            e = len(nums)-1
-            while f < e:
-                if nums[f]+nums[e] == target:
-                    new_ans = (nums[i], nums[f], nums[e])
-                    if new_ans not in ans_set:
-                        ans_set.add(new_ans)
-                        ans.append([nums[i], nums[f], nums[e]])
-                    f += 1
-                    e -= 1
-                    continue
-
-                if nums[f]+nums[e] < target:
-                    f += 1
-                    continue
-                else:
-                    e -= 1
-                    
-        return ans
-                    
+        
+        
