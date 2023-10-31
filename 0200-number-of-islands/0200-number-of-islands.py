6eb62e43240be07a04f24adc63faaf2a116db6d2
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-31 16:43:10 -0400

    Time: 280 ms (71.04%), Space: 18.5 MB (98.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(i, j):
-            dir = [(1, 0),(0, 1),(-1, 0),(0, -1)]
-            q = deque([(i, j)])
-            while q:
-                cur_x, cur_y = q.popleft()
-                for dx, dy in dir:
-                    x, y = cur_x + dx, cur_y + dy
-                    if x >= 0 and y >= 0 and x < len(grid) and y < len(grid[0]):
-                        if grid[x][y] == '1':
-                            grid[x][y] = '0'
-                            q.append((x, y))
-                                
-
+        m = len(grid)
+        n = len(grid[0])
         count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == '1':
                     count += 1
-                    bfs(i, j)
+                    self.bfs(grid, i, j)
         return count
+    
+    def bfs(self, grid, i, j):
+        q = deque()
+        q.append((i, j))
+        grid[i][j] = '2'
         
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        while q:
+            cx, cy = q.popleft()
+            for dx, dy in dirs:
+                nx, ny = cx + dx, cy + dy
+                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == '1':
+                    q.append((nx, ny))
+                    grid[nx][ny] = '2'
+        
+            
         
         
