83f8d87a85843bcce7498e6d8ab4a5ce7d5d2f05
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-31 14:29:59 -0400

    Time: 40 ms (41.47%), Space: 16.3 MB (9.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,43 @@
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+        
+#         dp = defaultdict(int)
+#         dp[1] = 1
+#         dp[2] = 2
+        
+#         for i in range(3, n+1):
+#             dp[i] = dp[i-2] + dp[i-1]
+#         return dp[n]
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         self.memo = {}
+#         return self.dp(n)
+
+#     def dp(self, i):
+#         if i <= 2: 
+#             return i
+#         if i not in self.memo:
+#             # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
+#             # store the result inside a hashmap to refer to in the future.
+#             self.memo[i] = self.dp(i - 1) + self.dp(i - 2)
+        
+#             return self.memo[i]
+
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return self.dp(n)
+        memo = {}
+        return self.dp(n, memo)
+
+    def dp(self, i, memo):
+        if i <= 2: 
+            return i
+        if i not in memo:
+            # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
+            # store the result inside a hashmap to refer to in the future.
+            memo[i] = self.dp(i - 1, memo) + self.dp(i - 2, memo)
         
-    
-    @cache
-    def dp(self, n):
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        else:
-            return self.dp(n-2) + self.dp(n-1)
+        return memo[i]
         
         
