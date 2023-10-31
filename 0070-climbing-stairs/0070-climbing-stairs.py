64fecca7f699aae50142e6bc83bcb578b6400895
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-10-31 14:30:19 -0400

    Time: 37 ms (59.12%), Space: 16.3 MB (39.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -9,35 +9,35 @@
 #             dp[i] = dp[i-2] + dp[i-1]
 #         return dp[n]
 
-# class Solution:
-#     def climbStairs(self, n: int) -> int:
-#         self.memo = {}
-#         return self.dp(n)
-
-#     def dp(self, i):
-#         if i <= 2: 
-#             return i
-#         if i not in self.memo:
-#             # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
-#             # store the result inside a hashmap to refer to in the future.
-#             self.memo[i] = self.dp(i - 1) + self.dp(i - 2)
-        
-#             return self.memo[i]
-
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        return self.dp(n, memo)
+        self.memo = {}
+        return self.dp(n)
 
-    def dp(self, i, memo):
+    def dp(self, i):
         if i <= 2: 
             return i
-        if i not in memo:
+        if i not in self.memo:
             # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
             # store the result inside a hashmap to refer to in the future.
-            memo[i] = self.dp(i - 1, memo) + self.dp(i - 2, memo)
+            self.memo[i] = self.dp(i - 1) + self.dp(i - 2)
         
-        return memo[i]
+        return self.memo[i]
+
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         memo = {}
+#         return self.dp(n, memo)
+
+#     def dp(self, i, memo):
+#         if i <= 2: 
+#             return i
+#         if i not in memo:
+#             # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
+#             # store the result inside a hashmap to refer to in the future.
+#             memo[i] = self.dp(i - 1, memo) + self.dp(i - 2, memo)
+        
+#         return memo[i]
         
         
