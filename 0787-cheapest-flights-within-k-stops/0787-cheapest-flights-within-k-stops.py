ffe024c2abb4be2887ae82054fd2e9f4fba78622
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-11-01 22:01:02 -0400

    Time: 94 ms (96.16%), Space: 18 MB (14.61%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,19 +1,20 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph, prices = defaultdict(list), [-1 for _ in range(n)]
+        graph, stops = defaultdict(list), [float('inf')]*n
         for f, to, price in flights:
             graph[f].append([to, price])
-        prices[src], q, step = 0, deque([src]), 0
-        while q and step <= k:
-            sz = len(q)
-            new_prices = list(prices)
-            for _ in range(sz):
-                cur = q.popleft()
-                for neighbor, price in graph[cur]:
-                    if new_prices[neighbor] == -1 or new_prices[neighbor] > prices[cur]+price:
-                        new_prices[neighbor] = prices[cur] + price
-                        q.append(neighbor)
-            step += 1
-            prices = new_prices
-        return prices[dst]
+            
+        q = [(0, src, 0)]
+        while q:
+            dist, node, steps = heapq.heappop(q)
+            if steps > stops[node] or steps > k + 1:
+                continue
+            stops[node] = steps
+            if node == dst:
+                return dist
+            if node not in graph:
+                continue
+            for neighbor, price in graph[node]:
+                heapq.heappush(q, (dist+price, neighbor, steps+1))
+        return -1
         
