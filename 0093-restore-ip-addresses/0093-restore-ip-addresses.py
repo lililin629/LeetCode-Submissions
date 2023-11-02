d318188448c5a2fecc8b505fd54cc6ddbd83beee
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-11-01 22:47:27 -0400

    Time: 39 ms (77.40%), Space: 16.3 MB (35.14%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,41 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        n = len(s)
+        # 4 <= n <= 12
+        # [1-3]*4
+        ans = []
+        self.dfs(ans, [], s, 0)
+        return ans
+        
+    def dfs(self, ans, ip, s, idx):
+        if len(ip) == 4 and idx == len(s):
+            sip = '.'.join(ip)
+            ans.append(sip)
+            return
+        
+        if len(ip) == 4 and idx != len(s):
+            return
+        
+        for i in range(1,4):
+            if idx+i > len(s):
+                break
+            st = s[idx:idx+i]
+            if i > 1 and s[idx] == '0':
+                continue
+            if int(st) > 255:
+                continue
+            
+                
+            ip.append(s[idx:idx+i])
+            self.dfs(ans, ip, s, idx+i)
+            ip.pop()
+        
+        
+        
+        
+        
+        
+        
+    
+        
+        
