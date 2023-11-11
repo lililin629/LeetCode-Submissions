a9f46161ae6582b4c2e2bcf4b7fcc6b42b7cda58
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-11-11 09:07:31 -0500

    Time: 262 ms (18.22%), Space: 91.4 MB (7.09%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        ans = 0 
+        memo = defaultdict(bool)
+        for i in range(len(s)):
+            memo[(i, i)] = True
+            ans += 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                memo[(i-1, i)] = True
+                ans += 1
+              
+        for length in range(2, len(s)):
+            for i in range(len(s) - length):
+                j = i + length
+                if s[i] == s[j] and memo[(i + 1, j - 1)]:
+                    memo[(i, j)] = True
+                    ans += 1
+        return ans
+                
+                    
+                
+        
