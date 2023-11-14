96ede81abf90986de8d61fb34860b3578e456a13
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-11-14 12:40:18 -0500

    Time: 1320 ms (13.56%), Space: 201.7 MB (16.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -3,13 +3,12 @@ class Solution:
         l1 = len(text1)
         l2 = len(text2)
         memo = defaultdict(int)
-        for i in range(l1-1, -1, -1):
-           
-            for j in range(l2-1, -1, -1):
+        for i in range(l1):
+            for j in range(l2):
                 if text1[i] == text2[j]:
-                    memo[(i, j)] = 1 + memo[(i+1, j+1)]
+                    memo[(i, j)] = 1 + memo[(i-1, j-1)]
                 else:
-                    memo[(i, j)] = max(memo[(i+1, j)], memo[(i, j+1)])
-        return memo[(0, 0)]
+                    memo[(i, j)] = max(memo[(i-1, j)], memo[(i, j-1)])
+        return memo[(l1-1, l2-1)]
                 
         
