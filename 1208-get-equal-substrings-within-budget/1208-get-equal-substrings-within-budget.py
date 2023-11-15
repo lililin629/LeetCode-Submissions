d8eaf4001f62f8e70f44c3628ae561d448f9ae87
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-11-15 14:07:30 -0500

    Time: 88 ms (18.36%), Space: 27.6 MB (5.67%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -0,0 +1,30 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        n = len(s)
+        dist = defaultdict(int)
+        
+        for i in range(n):
+            dist[i] = abs(ord(s[i]) - ord(t[i]))
+        
+        print(dist)
+        l = r = 0
+        dif = 0
+        mx = 0
+        
+        while r < n:
+            dif += dist[r]
+            if dif <= maxCost:
+                mx = max(mx, r-l+1)
+            else:
+                dif -= dist[l]
+                l += 1
+            r += 1
+        return mx
+                
+            
+            
+        
+       
+                
+                
+        
