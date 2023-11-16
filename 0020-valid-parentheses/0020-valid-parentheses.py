c29988878353b30c4615b858c2ffc5f8b9e89044
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-11-16 15:58:05 -0500

    Time: 26 ms (99.12%), Space: 16.4 MB (18.13%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,59 +1,34 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # (({}))
+        
+        # {}(())
+        
+        # {(})
+        
+        # {)
+        
         st = []
-        dic = {')':'(', 
-               ']':'[',
-               '}':'{'}
-        for ch in s:
-            if ch in dic:
-                if st and st[-1] == dic[ch]:
+        left = {'(', '{', '['}
+        pair = {')':'(', '}':'{', ']':'['}
+        
+        for br in s:
+            if br in left:
+                st.append(br)
+            else:
+                if st and st[-1] == pair[br]:
                     st.pop()
                 else:
                     return False
-            else:
-                st.append(ch)
         if not st:
             return True
         return False
                 
+        
+                
                 
         
         
         
         
-        
-        
-        
-#         self.st = []
-#         # FILO
-#         for ch in s:
-#             if ch == '(' or ch == '{' or ch == '[':
-#                 self.st.append(ch)
-#             if ch == ')':
-#                 if self.check(ch, '('):
-#                     continue
-#                 return False
-#             if ch == ']':
-#                 if self.check(ch, '['):
-#                     continue
-#                 return False
-#             if ch == '}':
-#                 if self.check(ch, '{'):
-#                     continue
-#                 return False
-                
-            
-#         if len(self.st) != 0:
-#             return False
-#         return True
-    
-#     def check(self, ch, lp):
-#         if self.st and self.st[-1] == lp:
-#             self.st.pop()
-#             return True
-#         return False
-        
-            
-                
-            
         
