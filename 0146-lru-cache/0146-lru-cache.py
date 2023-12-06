d9ae275aa4c160d920332e66423045c82a8d0720
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-12-06 11:11:53 -0500

    Time: 732 ms (24.60%), Space: 78.5 MB (19.99%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,100 +1,52 @@
-class LinkedNode:
-    def __init__(self, key, val):
+class Node:
+    def __init__(self, key, value):
         self.key = key
-        self. val = val
-        self.next = None
+        self.value = value
         self.prev = None
+        self.next = None
 
-class LRUCache(object):
+class LRUCache:
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
+    def __init__(self, capacity: int):
         self.cap = capacity
-        self.dict = {} # {(key: node)}
-        self.dummy = LinkedNode(0,0) 
-        self.tail = self.dummy
-        
+        self.cache = {}  # Maps keys to Node objects
+        self.left = Node(0, 0)  # Left (oldest) sentinel node
+        self.right = Node(0, 0)  # Right (most recent) sentinel node
+        self.left.next = self.right
+        self.right.prev = self.left
 
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        # if key not in cache
-        if key not in self.dict:
-            return -1
-        # if key in cache
-        else:
-            val = self.dict[key].val
-            # move node to end
-            self.kick(key)
-            
-            # return val
-            return val
-    
-    def kick(self, key):
-        keyNode = self.dict[key]
-        prev = keyNode.prev
-        if not keyNode.next:
-            return
-        prev.next = keyNode.next
-        keyNode.next = None
-        self.tail.next = keyNode
-        keyNode.prev = self.tail
-        prev.next.prev = prev
-        self.tail = keyNode
-    
+    def _remove(self, node):
+        """Remove a node from the linked list."""
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
 
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        # if key in dict
-        if key in self.dict:
-            # update value
-            self.dict[key].val = value
-            # move to last
-            self.kick(key)
-        # if key not in dict
-        else:
-            keyNode = LinkedNode(key, value)
-            # if not at capacity
-            if len(self.dict) < self.cap:
-                # update dict
-                self.dict[key] = keyNode
-                # add to last
-                self.tail.next = keyNode
-                keyNode.prev = self.tail
-                self.tail = keyNode
+    def _insert(self, node):
+        """Insert a node to the right (most recent) end of the linked list."""
+        prev = self.right.prev
+        next = self.right
+        prev.next = node
+        node.prev = prev
+        node.next = next
+        next.prev = node
 
-            # if at capacity
-            else:
-                deleted_key = self.dummy.next.key
-                if self.dummy.next.next:
-                    # replace dummy.next
-                    self.dummy.next =  self.dummy.next.next
-                    self.dummy.next.prev = self.dummy
-                    # add to last
-                    self.tail.next = keyNode
-                    keyNode.prev = self.tail
-                    self.tail = keyNode
-                else:
-                    # replace dummy.next
-                    self.dummy.next = keyNode
-                    keyNode.prev = self.dummy
-                    self.tail = keyNode
-                # update dict
-                self.dict.pop(deleted_key)
-                self.dict[key] = keyNode
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self._remove(self.cache[key])
+            self._insert(self.cache[key])
+            return self.cache[key].value
+        return -1
 
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self._insert(self.cache[key])
 
+        if len(self.cache) > self.cap:
+            # Remove from the linked list and delete the LRU from the cache
+            lru = self.left.next
+            self._remove(lru)
+            del self.cache[lru.key]
 
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
