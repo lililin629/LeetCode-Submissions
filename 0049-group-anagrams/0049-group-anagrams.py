c1f41c4a6942dfcc69433c4f2af55ade339252d4
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-12-17 18:30:53 -0500

    Time: 147 ms (5.63%), Space: 19.7 MB (70.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # sort every string
-        ans = []
-        strs_map = {}
+        d = defaultdict(list)
         for s in strs:
-            ls = []
-            tp = tuple()
-            for ch in s:
-                ls.append(ch)
-                ls.sort()
-                tp = tuple(ls)
-            if tp not in strs_map:
-                strs_map[tp] = [s]
-            else:
-                strs_map[tp].append(s)
-        for tp in strs_map:
-            ans.append(strs_map[tp])
+            ds = Counter(s)
+            st = ''
+            for c, count in sorted(ds.items()):
+                st += str(count)
+                st += c
+            d[st].append(s)
+        
+        ans = []
+        for st, lst in d.items():
+            ans.append(lst)
         return ans
                 
-                
-            
             
         
         
