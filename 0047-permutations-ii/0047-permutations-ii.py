247e5c6769de3fabd0ddf082492f85090e54d02d
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-12-18 17:04:43 -0500

    Time: 50 ms (95.46%), Space: 16.5 MB (88.75%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums]
+        else:
+            nums.sort()
+            ans = []
+            self.perm(ans, nums, set(), [])
+            return ans
+    
+    def perm(self, ans, nums, visited, cur):
+        if len(cur) == len(nums):
+            ans.append(cur)
+            return
+        
+        for i in range(len(nums)):
+            if i in visited:
+                continue
+            if i > 0 and nums[i] == nums[i-1] and (i-1) not in visited:
+                continue
+                
+            
+            visited.add(i)
+            self.perm(ans, nums, visited, cur+[nums[i]])
+            visited.remove(i)
+            
+        
+        
