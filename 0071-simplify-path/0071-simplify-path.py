109077e6877f9da795ad6642e227cdd5c30986e7
lililin629 <42260704+lililin629@users.noreply.github.com>
2023-12-20 09:13:08 -0500

    Time: 36 ms (82.12%), Space: 17.3 MB (18.23%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,28 +1,3 @@
-# class Solution:
-#     def simplifyPath(self, path: str) -> str:
-#         if path[-1] == "/":
-#             path = path[:-1]
-#         if path[0] != "/":
-#             path = "/"+ path
-        
-#         st = []
-#         for i in range(len(path)):
-#             print(st)
-#             if path[i] == "/":
-#                 if not st or st[-1] != "/":
-#                     st.append(path[i])
-#             elif path[i] == ".":
-#                 if i > 0 and path[i-1] == ".":
-#                     times = 2
-#                     while st and times:
-#                         st.pop()
-#                         times -= 1
-#             else:
-#                 st.append(path[i])
-#         ans = "".join(st)
-        
-#         return ans
-            
 class Solution:
     def simplifyPath(self, path: str) -> str:
         st = []
