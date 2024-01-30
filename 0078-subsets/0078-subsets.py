3acfe738b6d890199f8b0bb1edbe103094b64582
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-01-30 18:55:23 -0500

    Time: 32 ms (90.66%), Space: 16.7 MB (71.42%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,34 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        k = len(nums)-1
-        return self.dfs(k, nums)
-    
-    
-    def dfs(self, k, nums):
-        if k < 0:
-            return [[]]
-        pre = self.dfs(k-1, nums)
         ans = []
-        for ls in pre:
-            ans.append(ls+[nums[k]])
-        return pre+ans
-        
-        
-       
-#         self.ans = []
-#         for l in range(len(nums)+1):
-#             self.dfs(l, 0, nums, [])
-#         return self.ans
-        
+        n = len(nums)
+        for i in range(n+1):
+            self.helper(0, nums, [], i, ans)
+        return ans
+            
     
-#     def dfs(self, l, pointer, nums, cur):
-#         if len(cur) == l:
-#             self.ans.append(list(cur))
-#             return
+    def helper(self, start, nums, com, length, ans):
+        if len(com) == length:
+            ans.append(list(com))
+        for i in range(start, len(nums)):
+            com.append(nums[i])
+            self.helper(i+1, nums, com, length, ans)
+            com.pop()
         
-#         for j in range(pointer, len(nums)):
-#             cur.append(nums[j])
-#             self.dfs(l, j+1, nums, cur)
-#             cur.pop()
         
+            
+            
         
