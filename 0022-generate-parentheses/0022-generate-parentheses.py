510ecc4f9508b7ca10b29b9c0297dcd1cb490acd
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-01-31 17:28:10 -0500

    Time: 36 ms (77.68%), Space: 16.8 MB (64.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,24 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-        self.helper(0, 0, n, '', ans)
+        self.helper(0, n, '', ans)
         return ans
     
-    def helper(self, start, left, n, cur, ans):
-        if len(cur) == 2*n and left==0:
-            ans.append(cur)
-        if start > 2*n:
+    def helper(self, left, n, cur, ans):
+        if len(cur) == 2*n:
+            if left==0:
+                ans.append(cur)
             return
+        # if start > 2*n:
+        #     return
         if left > 0:
             # +left
-            self.helper(start+1, left+1, n, cur+'(', ans)
+            self.helper(left+1, n, cur+'(', ans)
             # +right
-            self.helper(start+1, left-1, n, cur+')', ans)
+            self.helper(left-1, n, cur+')', ans)
         
         else:
-            self.helper(start+1, left+1, n, cur+'(', ans)
+            self.helper(left+1, n, cur+'(', ans)
             
             
       
