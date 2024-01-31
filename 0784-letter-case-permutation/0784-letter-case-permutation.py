9a4e8557013a8bfba55dc416343480085e9758e2
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-01-31 16:43:23 -0500

    Time: 77 ms (18.28%), Space: 17.3 MB (91.22%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,3 +1,21 @@
+# class Solution:
+#     def letterCasePermutation(self, s: str) -> List[str]:
+#         ans = set()
+#         self.helper(0, s, ans, '')
+#         return list(ans)
+    
+#     def helper(self, start, s, ans, cur):
+#         if len(cur) == len(s):
+#             ans.add(cur)
+#         if start >= len(s):
+#             return
+#         if len(cur) - start < 0:
+#             return
+            
+#         self.helper(start+1, s, ans, cur+s[start].lower())
+#         self.helper(start+1, s, ans, cur+s[start].upper())
+
+
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         ans = set()
@@ -11,9 +29,10 @@ class Solution:
             return
         if len(cur) - start < 0:
             return
-            
-        self.helper(start+1, s, ans, cur+s[start].lower())
-        self.helper(start+1, s, ans, cur+s[start].upper())
+        
+        for i in range(start, len(s)):    
+            self.helper(i+1, s, ans, cur+s[i].lower())
+            self.helper(i+1, s, ans, cur+s[i].upper())
             
             
 
