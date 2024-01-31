6ff68ce858646f333f4947bdc5412c5193093334
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-01-31 15:38:12 -0500

    Time: 37 ms (81.79%), Space: 16.7 MB (77.33%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,41 +1,37 @@
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # Sort the input list
-        ans = []
-        self.helper(0, nums, [], ans)
-        return ans
-    
-    def helper(self, start, nums, cur, ans):
-        ans.append(list(cur))
-        for i in range(start, len(nums)):
-            # Skip duplicates
-            if i > start and nums[i] == nums[i - 1]:
-                continue
-            cur.append(nums[i])
-            self.helper(i + 1, nums, cur, ans)
-            cur.pop()
-
 #     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()  # Sort the input list
 #         ans = []
-#         nums.sort()
-#         for length in range(len(nums)+1):
-#             self.helper(0, nums, [], ans, length)
+#         self.helper(0, nums, [], ans)
 #         return ans
     
-#     def helper(self, start, nums, cur, ans, length):
-#         # if len(cur) + len(nums) - start < length:
-#         #     return
-#         if start >= len(nums):
-#             return
-#         if len(cur) == length:
-#             ans.append(list(cur))
-        
+#     def helper(self, start, nums, cur, ans):
+#         ans.append(list(cur))
 #         for i in range(start, len(nums)):
-#             if i != start and nums[i] == nums[i-1]:
+#             # Skip duplicates
+#             if i > start and nums[i] == nums[i - 1]:
 #                 continue
 #             cur.append(nums[i])
-#             self.helper(i+1, nums, cur, ans, length)
+#             self.helper(i + 1, nums, cur, ans)
 #             cur.pop()
+
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        for length in range(len(nums)+1):
+            self.helper(0, nums, [], ans, length)
+        return ans
+    
+    def helper(self, start, nums, cur, ans, length):
+        if len(cur) == length:
+            ans.append(list(cur))
+        
+        for i in range(start, len(nums)):
+            if i != start and nums[i] == nums[i-1]:
+                continue
+            cur.append(nums[i])
+            self.helper(i+1, nums, cur, ans, length)
+            cur.pop()
             
         
         
