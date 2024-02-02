6a9fa4b27bb80a752c9044a8151528c146424641
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-02-02 08:34:11 -0500

    Time: 26 ms (97.97%), Space: 16.5 MB (61.73%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,30 +1,29 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        d = {
+             '2':['a', 'b', 'c'],
+             '3':['d', 'e', 'f'],
+             '4':['g', 'h', 'i'],
+             '5':['j', 'k', 'l'],
+             '6':['m', 'n', 'o'],
+             '7':['p', 'q', 'r', 's'],
+             '8':['t', 'u', 'v'],
+             '9':['w', 'x', 'y', 'z']
+            }
         if not digits:
             return []
-        self.d = {
-            "2":["a","b","c"],
-            "3":["d","e","f"], 
-            "4":["g","h","i"], 
-            "5":["j","k","l"], 
-            "6":["m","n","o"], 
-            "7":["p","q","r","s"], 
-            "8":["t","u","v"], 
-            "9":["w","x","y","z"]
-            }
-        combos = []
-        self.dfs(digits, 0, '', combos)
-        return combos
-    
-    def dfs(self, digits, idx, combo, combos):
         
-        if idx == len(digits):
-            combos.append(combo)
+        ans = []
+        self.helper(digits, d, 0, '', ans)
+        return ans
+        
+    def helper(self, digits, d, ind, cur, ans):
+        if len(cur) == len(digits):
+            ans.append(cur)
             return
         
-        for ch in self.d[digits[idx]]:
-            self.dfs(digits, idx+1, combo+ch, combos)
+        for c in d[digits[ind]]:
+            self.helper(digits, d, ind+1, cur+c, ans)
             
-        
-        
+            
         
