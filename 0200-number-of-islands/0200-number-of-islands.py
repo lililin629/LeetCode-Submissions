1678440505dceec13e75919e74b781af4194e2a1
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-02-02 15:32:06 -0500

    Time: 256 ms (62.53%), Space: 24 MB (40.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,52 +1,37 @@
-# class Solution:
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         m = len(grid)
-#         n = len(grid[0])
-#         count = 0
-#         for i in range(m):
-#             for j in range(n):
-#                 if grid[i][j] == '1':
-#                     count += 1
-#                     self.bfs(grid, i, j)
-#         return count
-    
-#     def bfs(self, grid, i, j):
-#         q = deque()
-#         q.append((i, j))
-#         grid[i][j] = '2'
-        
-#         dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        
-#         while q:
-#             cx, cy = q.popleft()
-#             for dx, dy in dirs:
-#                 nx, ny = cx + dx, cy + dy
-#                 if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == '1':
-#                     q.append((nx, ny))
-#                     grid[nx][ny] = '2'
-                    
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
+        visited = set()
         count = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
+                if grid[i][j] == '1' and (i, j) not in visited:
                     count += 1
-                    self.dfs(grid, i, j)
+                    self.bfs(grid, visited, i, j, m, n)
         return count
     
-    def dfs(self, grid, i, j):
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        grid[i][j] = '2'
+    def bfs(self, grid, visited, i, j, m, n):
+        q = deque([(i, j)])
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while q:
+            cx, cy = q.popleft()
+            for (dx, dy) in dirs:
+                nx, ny = cx + dx, cy + dy
+                if (nx, ny) not in visited and 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == '1':
+                    q.append((nx, ny))
+                    visited.add((nx, ny))
+                    
+                    
+         
         
-        for dx, dy in dirs:
-            nx, ny = i+dx, j+dy
-            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == '1':
-                self.dfs(grid, nx, ny)
+        
+        
             
-            
+        
+        
+        
+        
         
         
             
