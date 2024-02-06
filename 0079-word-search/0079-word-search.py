98651816b19fc4fa9e4f45decf497f907c096d19
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-02-05 20:39:16 -0500

    Time: 3985 ms (70.00%), Space: 16.4 MB (86.82%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,33 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.row = len(board)
-        self.col = len(board[0])
-        self.board = board
-        for i in range(self.row):
-            for j in range(self.col):
-                if self.backtrack(i, j, word):
-                    return True
-    
-    def backtrack(self, row, col, word):
-        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        if not len(word):
-            return True
-        if not self.valid(row, col, word):
-            return False
-        
-        self.board[row][col] = '-1'
-        for dx, dy in dirs:
-            nr, nc = row+dx, col+ dy
-            if self.backtrack(nr, nc, word[1:]):
-                return True
-        self.board[row][col] = word[0]
+        m = len(board)
+        n = len(board[0])
+        self.dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        visited = set()
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0] and (i, j) not in visited:
+                    visited.add((i, j))
+                    if self.dfs(board, word, i, j, visited, m, n, 0):
+                        return True
+                    visited.remove((i, j))
         return False
     
-    def valid(self, r, c, word):
-        if r >= self.row or r < 0 or c >= self.col or c < 0 or self.board[r][c] != word[0]:
-            return False
-        return True
-                    
-            
+    def dfs(self, board, word, i, j, visited, m, n, idx):
+        
+        if idx == len(word)-1:
+            return True
+        
+        for (dx, dy) in self.dirs:
+            (nx, ny) = (i+dx, j+dy)
+            if 0 <= nx < m and 0 <= ny < n and (nx, ny) not in visited and board[nx][ny] == word[idx+1]:
+                visited.add((nx, ny))
+                if self.dfs(board, word, nx, ny, visited, m, n, idx+1):
+                    return True
+                visited.remove((nx, ny))
+        return False
             
         
         
+        
+                
