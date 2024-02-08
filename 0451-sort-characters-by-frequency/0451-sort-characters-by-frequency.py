5fe3091912ac345c7d5e4919da9f6db730e94128
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-02-08 10:41:21 -0500

    Time: 38 ms (91.42%), Space: 17.7 MB (93.31%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -2,31 +2,42 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         count = Counter(s)
         lst = [(ct, ch) for (ch,ct) in count.items()]
-        
-        n = len(lst)
-        self.qs(0, n-1, lst)
+        lst.sort(reverse= True)
         ans = ''
-        for (ct, ch) in lst[::-1]:
-            ans += ch*ct
+        for (ct, ch) in lst:
+            ans += ct*ch
         return ans
-    
-    def qs(self, start, end, lst):
-        if start >= end:
-            return
-        l, r = start, end
-        piv = lst[(l+r)//2]
-        while l <= r:
-            while l <= r and lst[r] > piv:
-                r -= 1
-            while l <= r and lst[l] < piv:
-                l += 1 
             
-            if l <= r:
-                lst[l], lst[r] = lst[r], lst[l]
-                r -= 1
-                l += 1
-        self.qs(start, r, lst)
-        self.qs(l, end, lst)
+        
+# class Solution:
+#     def frequencySort(self, s: str) -> str:
+#         count = Counter(s)
+#         lst = [(ct, ch) for (ch,ct) in count.items()]
+        
+#         n = len(lst)
+#         self.qs(0, n-1, lst)
+#         ans = ''
+#         for (ct, ch) in lst[::-1]:
+#             ans += ch*ct
+#         return ans
+    
+#     def qs(self, start, end, lst):
+#         if start >= end:
+#             return
+#         l, r = start, end
+#         piv = lst[(l+r)//2]
+#         while l <= r:
+#             while l <= r and lst[r] > piv:
+#                 r -= 1
+#             while l <= r and lst[l] < piv:
+#                 l += 1 
+            
+#             if l <= r:
+#                 lst[l], lst[r] = lst[r], lst[l]
+#                 r -= 1
+#                 l += 1
+#         self.qs(start, r, lst)
+#         self.qs(l, end, lst)
                 
             
             
