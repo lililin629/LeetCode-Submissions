d8a4f8daa0c2ec79af72e90efcb0d3ab2f293f44
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-02-29 11:54:29 -0500

    Time: 3678 ms (5.49%), Space: 16.5 MB (99.96%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,60 +1,33 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        d = defaultdict(list)
+        count = 0
+        for i, b in enumerate(bombs):
+            count = max(self.bfs(i, bombs), count)
+        return count
+    
+    def bfs(self, i, bombs):
+        q = deque()
+        [x, y, r] = bombs[i]
+        q.append((x, y, r))
+        used = set()
+        used.add((i, x, y))
+        count = 1
         
-        for i, [x,y,r] in enumerate(bombs):
-            for j, [nx, ny, nr] in enumerate(bombs):
-                if i != j:
-                    if (x-nx)**2 + (y-ny)**2 <= r**2:
-                        d[i].append(j)
-                        
-        max_total = 0
-        for bomb in range(len(bombs)):
-            visited = set()
-            max_total = max(self.dfs(bomb, d, visited), max_total)
-        return max_total
+        while q:
+            cx, cy, cr = q.popleft()
+            
+            for j, [a, b, c] in enumerate(bombs):
+                if (j, a, b) not in used and self.dist(a,b,cx,cy) <= cr:
+                    q.append((a, b, c))
+                    used.add((j, a, b))
+                    count += 1
+        return count
+    
+    def dist(self, a, b, c, d):
+        return ((a-c)**2 + (b-d)**2)**(1/2)
         
-    def dfs(self, start, d, visited):
-        visited.add(start)
-       
-        for b in d[start]:
-            if b not in visited:
-                self.dfs(b, d, visited)               
-        return len(visited)
-
-# class Solution:
-#     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-#         d = defaultdict(list)
-        
-#         for i, [x,y,r] in enumerate(bombs):
-#             for j, [nx, ny, nr] in enumerate(bombs):
-#                 if i != j:
-#                     if (x-nx)**2 + (y-ny)**2 <= r**2:
-#                         d[i].append(j)
-#                     if (x-nx)**2 + (y-ny)**2 <= nr**2:
-#                         d[j].append(i)
-#         # {0:[1, 2], 1:[0, 2], 2:[1, 3]}
-#         max_total = 0
-#         for bomb in d:
-#             self.visited = set()
-#             max_total = max(self.dfs(bomb, d, 1), max_total)
-#         return max_total
-        
-#     def dfs(self, start, d, total):
-#         if start not in d:
-#             return total
-#         self.visited.add(start)
-#         for b in d[start]:
-#             if b not in self.visited:
-#                 self.dfs(b, d, total + 1)
-        
-        
-        
-                
-        
-
+                    
+                    
+            
             
         
