c474d476c46b61f48fc8073eaf065d2eab516f16
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-03-18 12:06:55 -0400

    Time: 37 ms (47.48%), Space: 16.5 MB (88.19%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
@@ -6,29 +6,25 @@
 #         self.right = right
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = defaultdict(list)
+        self.dfs(root, ans)
+        # change to list
+        ans_lst = []
+        for key in ans:
+            ans_lst.append(ans[key])
+        return ans_lst
+    
+    def dfs(self, root, ans):
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            ans[0].append(root.val)
+            return 1
+        l_lev = self.dfs(root.left, ans)
+        r_lev = self.dfs(root.right, ans)
+        lev = max(l_lev, r_lev)
+        ans[lev].append(root.val)
         
-        ans = {}
-        ret = []
+        return lev+1
         
-        def dfs(root): # returns (height, val)
-            if not root:
-                return -1, 0
-            else:
-                lh, l = dfs(root.left)
-                rh, r = dfs(root.right)
-                h, v = max(lh, rh)+1, root.val
-                if h not in ans:
-                    ans[h] = [v]
-                else:
-                    ans[h].append(v)
-                return h, v
-            
-        dfs(root)
-        sorted(ans.items())
-        
-        for h in ans:
-            ret.append(ans[h])
-            
-        return ret
-                    
         
