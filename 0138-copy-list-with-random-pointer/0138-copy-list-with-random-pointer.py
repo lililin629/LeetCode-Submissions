e5b93f36a75412de3d7f5c2e6d49691270c7d16b
lililin629 <42260704+lililin629@users.noreply.github.com>
2024-03-25 11:08:30 -0400

    Time: 36 ms (72.73%), Space: 17.6 MB (8.10%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,42 +11,41 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        new = Node(head.val)
-        
-        cur = head
-        new_cur = new
         d = dict()
-        nd = dict()
-        idx = 0
-        
-        while cur:
-            d[cur] = idx
-            nd[idx] = new_cur
-            
-            idx += 1
-            
-            if cur.next:
-                new_cur.next = Node(cur.next.val)
-            
-            cur = cur.next
-            new_cur = new_cur.next
-        
         cur = head
-        new_cur = new
+        new_head = Node(cur.val)
+        d[head] = new_head
         
+        
+        cur2 = new_head
+        cur = cur.next
+       
         while cur:
-            if cur.random:
-                key = d[cur.random]
-                new_cur.random = nd[key]
+            if cur in d:
+                new = d[cur]
             else:
-                new_cur.random = None
+                new = Node(cur.val)
+            d[cur] = new
+            cur2.next = new
+            cur2 = cur2.next
             cur = cur.next
-            new_cur = new_cur.next
-        
-        return new
-        
+    
+        cur = head
+        cur2 = new_head
+        while cur and cur2:
+            if cur.random == None:
+                cur2.random = None
+            else:
+                cur2.random = d[cur.random]
+            
+            cur2 = cur2.next
+            cur = cur.next
+        return new_head
+            
+       
+            
         
             
             
-            
+        
         
